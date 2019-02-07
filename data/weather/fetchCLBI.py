e68d5570a000dc245b01cace2b5927b4af1a5e27
giovaniceotto <ghceotto@gmail.com>
2019-02-07 18:08:28 +0800

    Update jupyter notebooks and associated files

--- data/weather/fetchCLBI.py
+++ data/weather/fetchCLBI.py
@@ -1,30 +1,56 @@
-#!/usr/bin/env python
-import calendar
-from ecmwfapi import ECMWFDataServer
-server = ECMWFDataServer()
- 
-def retrieve_interim():
-    """      
-        An ERA interim request for analysis pressure level data.
-        Change the keywords below to adapt it to your needs.
-        (eg to add or to remove  levels, parameters, times etc)
-        Request cost per day is 112 fields, 14.2326 Mbytes
-    """
-    server.retrieve({
-        "class": "ei",
-        "stream": "oper",
-        "type": "an",
-        "dataset": "interim",
-        "date": "2013-01-01/to/2017-10-31",
-        "expver": "1",
-        "levtype": "pl",
-        "levelist": "600/650/700/750/775/800/825/850/875/900/925/950/975/1000",
-        "param": "129.128/131.128/132.128/133.128",
-        "target": "CLBI.nc",
-        "format": "netcdf",
-        "time": "00/06/12/18",
-        "grid": "0.75/0.75",
-        "area": "-4.5/324/-7.5/326.25"
-    })
-if __name__ == '__main__':
-    retrieve_interim()
+import cdsapi
+
+c = cdsapi.Client()
+
+years = ['2015', '2016', '2017', '2018']
+
+for year in years:
+    print()
+    print(year)
+    print()
+    c.retrieve(
+        'reanalysis-era5-pressure-levels',
+        {
+            'product_type':'reanalysis',
+            'format':'netcdf',
+            'variable':[
+                'geopotential','temperature','u_component_of_wind',
+                'v_component_of_wind'
+            ],
+            'pressure_level':[
+                '600',
+                '650','700','750',
+                '775','800','825',
+                '850','875','900',
+                '925','950','975',
+                '1000'
+            ],
+            'year':[
+                year
+            ],
+            'month':[
+                '01','02','03',
+                '04','05','06',
+                '07','08','09',
+                '10','11','12'
+            ],
+            'day':[
+                '01','02','03',
+                '04','05','06',
+                '07','08','09',
+                '10','11','12',
+                '13','14','15',
+                '16','17','18',
+                '19','20','21',
+                '22','23','24',
+                '25','26','27',
+                '28','29','30',
+                '31'
+            ],
+            'time':[
+                '00:00','06:00','12:00',
+                '18:00'
+            ],
+            'area': '-4.5/324/-7.5/326.25'
+        },
+        'CLBI_' + year + '_ERA-5.nc')
