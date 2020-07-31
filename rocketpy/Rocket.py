3c5b9811e2c2971dbe92f300bcdce803469667ff
Guilherme Fernandes Alves <63590233+Gui-FernandesBR@users.noreply.github.com>
2020-07-31 01:37:04 -0300

    plot thrustToWeight graphs in rocket class

--- rocketpy/Rocket.py
+++ rocketpy/Rocket.py
@@ -66,7 +66,7 @@ class Rocket:
             mass without propellant.
         Rocket.thrustToWeight : Function
             Function of time expressing the motor thrust force divided by rocket
-            weight 
+            weight. The gravitational acceleration is assumed as 9.80665 m/s^2.
 
         Excentricity attributes:
         Rocket.cpExcentricityX : float
@@ -134,8 +134,7 @@ class Rocket:
                 Function of noisyPressureSignal.
             cleanPressureSignalFunction : Function
                 Function of cleanPressureSignal.
-
-       
+ 
         Aerodynamic attributes
         Rocket.aerodynamicSurfaces : list
             List of aerodynamic surfaces of the rocket.
@@ -270,7 +269,7 @@ class Rocket:
         # Calculate dynamic inertial quantities
         self.evaluateReducedMass()
         self.evaluateTotalMass()
-        self.thrustToWeight = self.rocket.motor.thrust()/(self.env.g() * self.rocket.totalMass())
+        self.thrustToWeight = self.motor.thrust()/(9.80665*self.totalMass())
 
         return None
 
@@ -892,6 +891,14 @@ class Rocket:
         self.staticMargin()
         self.powerOnDrag()
         self.powerOffDrag()
+        self.thrustToWeight()
+
+        #ax = plt.subplot(415)
+        #ax.plot(  , self.rocket.motor.thrust()/(self.env.g() * self.rocket.totalMass()))
+        #ax.set_xlim(0, self.rocket.motor.burnOutTime)
+        #ax.set_xlabel("Time (s)")
+        #ax.set_ylabel("Thrust/Weight")
+        #ax.set_title("Thrust-Weight Ratio")
 
         # Return None
         return None
