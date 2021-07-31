d905279845140a610b90415c459f98ed65a8388e
Guilherme Fernandes Alves <63590233+Gui-FernandesBR@users.noreply.github.com>
2021-07-30 21:57:55 -0300

    Adding author (me)

--- rocketpy/Environment.py
+++ rocketpy/Environment.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Giovani Hidalgo Ceotto"
+__author__ = "Giovani Hidalgo Ceotto, Guilherme Fernandes Alves"
 __copyright__ = "Copyright 20XX, Projeto Jupiter"
 __license__ = "MIT"
 
