cbc496896624189d9751181041a13ab53b0fff3b
ompro07 <66347921+ompro07@users.noreply.github.com>
2021-08-01 22:04:33 -0300

    Update SolidMotor.py

--- rocketpy/SolidMotor.py
+++ rocketpy/SolidMotor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Giovani Hidalgo Ceotto"
+__author__ = "Giovani Hidalgo Ceotto, Oscar Mauricio Prada Ramirez"
 __copyright__ = "Copyright 20XX, Projeto Jupiter"
 __license__ = "MIT"
 
