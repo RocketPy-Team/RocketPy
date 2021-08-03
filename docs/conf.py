58adf89e256ca7f430f61d6cd5268ce558f76fc4
patrick <patricksampaio@usp.br>
2021-08-03 19:24:55 -0300

    Replced png logo by svg format

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "static/logo.png"
+html_logo = "static/RocketPy_Logo_Black.svg"
 html_theme_options = {
     "icon_links": [
         {
