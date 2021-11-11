d40cfc77c309c1ab3a3fd1f1e872d7889fe5c517
FranzYuri <86535872+FranzYuri@users.noreply.github.com>
2021-11-11 16:35:20 -0300

    Update rocketpy/utilities.py
    Co-authored-by: Giovani Hidalgo Ceotto <ghceotto@gmail.com>

--- rocketpy/utilities.py
+++ rocketpy/utilities.py
@@ -4,23 +4,6 @@ __author__ = "Franz Masatoshi Yuri"
 __copyright__ = "Copyright 20XX, Projeto Jupiter"
 __license__ = "MIT"
 
-import re
-import math
-import bisect
-import warnings
-import time
-from datetime import datetime, timedelta
-from inspect import signature, getsourcelines
-from collections import namedtuple
-
-import numpy as np
-from scipy import integrate
-from scipy import linalg
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-
-from rocketpy import Environment, Rocket, SolidMotor, Flight
 
 
 def compute_cds_from_drop_test(final_speed, final_air_density, rocket_mass, gravity):
