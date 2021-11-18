12c79c8c019c6f79425d82ff90994a7eaf59d619
Mateus Stano <69485049+MateusStano@users.noreply.github.com>
2021-11-17 21:41:31 -0300

    Created class Analysis

--- rocketpy/Analysis.py
+++ rocketpy/Analysis.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
 
+__author__ = ""
+__copyright__ = "Copyright 20XX, Projeto Jupiter"
+__license__ = "MIT"
+
+class Analysis:
+    '''
+    
+    Documentation
+    
+    '''
+
+    def __init__(
+        self,
+        env,
+        motor,
+        rocket,
+        flight
+    ):
+        self.env = env
+        self.motor = motor
+        self.rocket = rocket
+        self.flight = flight
