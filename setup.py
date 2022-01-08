f458d30d04e885beb9056b23edb8cceb814a5abd
Lint Action <lint-action@samuelmeuli.com>
2022-01-08 23:59:59 +0000

    Fix code style issues with Black

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="rocketpy",
     version="0.9.9",
-    install_requires = [
-        'numpy>=1.0',
-        'scipy>=1.0',
-        'matplotlib>=3.0',
-        'requests',
-        'pytz',
+    install_requires=[
+        "numpy>=1.0",
+        "scipy>=1.0",
+        "matplotlib>=3.0",
+        "requests",
+        "pytz",
     ],
     maintainer="RocketPy Developers",
     author="Giovani Hidalgo Ceotto",
