fd25156a047b7485f82a341fdf337278bdbe7e7b
giovaniceotto <ghceotto@gmail.com>
2022-03-31 07:07:50 -0500

    MAINT: create new structure to parse surface files

--- rocketpy/EnvironmentAnalysis.py
+++ rocketpy/EnvironmentAnalysis.py
@@ -1,27 +1,50 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import numpy as np
+from matplotlib import pyplot as plt
+from windrose import WindAxes, WindroseAxes
 
 from rocketpy.Environment import Environment
 
-from windrose import WindroseAxes, WindAxes
-from matplotlib import pyplot as plt
-import numpy as np
-
 
 class EnvironmentAnalysis:
+    """Class for analyzing the environment.
+
+    List of ideas suggested by Logan:
+        - average max/min temperature
+        - record max/min temperature
+        - average max wind gust
+        - record max wind gust
+        - plot of wind gust distribution (should be Weibull)
+        - animation of who wind gust distribution evolves over average day
+        - temperature progression throughout the day at some fine interval (ex: 10 min) with 1, 2, 3, sigma contours (sketch below)
+        - average, 1, 2, 3 sigma wind profile from 0 - 35,000 ft AGL
+        - average day wind rose
+        - animation of how average wind rose evolves throughout an average day
+        - animation of how wind profile evolves throughout an average day
+
+    All items listed are relevant to either
+        1. participant safety
+        2. launch operations (range closure decision)
+        3. rocket performance
+
+    How does this class work?
+    - The class is initialized with a start date and end date.
+    - The class then parses the weather data from the start date to the end date.
+    - The class then calculates the average max/min temperature, average max wind gust, and average day wind rose.
+    - The class then plots the average max/min temperature, average max wind gust, and average day wind rose.
+    """
+
     def __init__(
         self,
-        railLength,
         start_date,
         end_date,
-        gravity=9.80665,
         latitude=0,
         longitude=0,
         elevation=0,
-        datum="SIRGAS2000",
         timeZone="UTC",
+        surfaceData=None,
+        pressureLevelData=None,
     ):
 
         # not the bast fashion
@@ -57,6 +80,57 @@ class EnvironmentAnalysis:
 
         self.average_max_wind_gust = 0
         self.maximum_wind_gust = 0
+
+    def __bilinear_interpolation(self, x, y, x1, x2, y1, y2, z11, z12, z21, z22):
+        """
+        Bilinear interpolation.
+
+        Source: GitHub Copilot
+        """
+        return (
+            z11 * (x2 - x) * (y2 - y)
+            + z21 * (x - x1) * (y2 - y)
+            + z12 * (x2 - x) * (y - y1)
+            + z22 * (x - x1) * (y - y1)
+        ) / ((x2 - x1) * (y2 - y1))
+
+    def parsePressureLevelData(self):
+        """
+        Parse pressure level data from a weather file.
+
+        Sources of information:
+        - https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels-preliminary-back-extension?tab=overview
+        - 
+
+        Must get the following variables:
+        - Geopotential
+        - U-component of wind
+        - V-component of wind
+        - Temperature
+        """
+        pass
+
+    def parseSurfaceData(self):
+        """
+        Parse surface data from a weather file.
+
+        Sources of information:
+        - https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-pressure-levels-preliminary-back-extension?tab=overview
+        - 
+
+        Must get the following variables:
+        - 2m temperature
+        - Surface pressure
+        - 10m u-component of wind
+        - 10m v-component of wind
+        - 100m u-component of wind
+        - 100m V-component of wind
+        - Instantaneous 10m wind gust
+        - Total precipitation
+        - Cloud base height
+
+        """
+        pass 
 
     def process_data(self):
         self.calculate_average_max_temperature()
