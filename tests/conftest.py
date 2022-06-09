b5a58b92c1a59170f3d25beafd75d786a7514306
Guilherme Fernandes Alves <guilherme_fernandes@usp.br>
2022-06-09 00:39:16 -0300

    adding missed variable to test.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,7 @@ def solid_motor():
         thrustSource="data/motors/Cesaroni_M1670.eng",
         burnOut=3.9,
         grainNumber=5,
+        distanceNozzleMotorReference=0.40396,
         grainSeparation=5 / 1000,
         grainDensity=1815,
         grainOuterRadius=33 / 1000,
