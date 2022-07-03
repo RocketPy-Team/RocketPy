3d9d69029a57a0ba517fc4ab1e08b2af4a796529
Guilherme Fernandes Alves <guilherme_fernandes@usp.br>
2022-07-02 22:43:33 -0300

    MAINT: modify copyright and imports

--- rocketpy/Rocket.py
+++ rocketpy/Rocket.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Giovani Hidalgo Ceotto, Franz Masatoshi Yuri"
-__copyright__ = "Copyright 20XX, Projeto Jupiter"
+__copyright__ = "Copyright 20XX, RocketPy Team"
 __license__ = "MIT"
 
-import re
-import math
-import bisect
-import warnings
-import time
-from datetime import datetime, timedelta
-from inspect import signature, getsourcelines
+
 from collections import namedtuple
+from inspect import getsourcelines
 
 import numpy as np
-from scipy import integrate
-from scipy import linalg
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from numpy import genfromtxt
 
 from .Function import Function
 from .Parachute import Parachute
