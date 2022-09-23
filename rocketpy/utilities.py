dae505669facb7227985b31c33ac0cc4ce2eb0eb
Franz Masatoshi Yuri <franz.yuri.ismart@gmail.com>
2022-09-23 20:13:00 -0300

    fixing errors and adding Valetudo.csv

--- rocketpy/utilities.py
+++ rocketpy/utilities.py
@@ -212,7 +212,6 @@ def create_dispersion_dictionary(dic):
     dictionary
         Dictionary with all rocket data used in dispersion analysis.
     """
-<<<<<<< HEAD
     try:
         file = np.genfromtxt(dic, usecols=(1, 2, 3), delimiter=",", dtype=str)
     except:
@@ -221,62 +220,11 @@ def create_dispersion_dictionary(dic):
     for list in file:
         if list[0] != "":
             if list[2] == "":
-                analysis_parameters[list[0]] = float(list[1])
+                try:
+                    analysis_parameters[list[0]] = float(list[1])
+                except:
+                    analysis_parameters[list[0]] = eval(list[1])
             else:
                 analysis_parameters[list[0]] = (float(list[1]), float(list[2]))
     return analysis_parameters
-=======
-    dataframe = pd.read_csv(dic, skiprows=[0, 1], header=None)
 
-    rocketKeys = list(dataframe[1].dropna())
-    rocketValues = list(dataframe[2].dropna())
-    rocketSD = list(dataframe[3])
-
-    motorKeys = list(dataframe[7].dropna())
-    motorValues = list(dataframe[8].dropna())
-    motorSD = list(dataframe[9])
-
-    launchKeys = list(dataframe[13].dropna())
-    launchValues = list(dataframe[14].dropna())
-    launchSD = list(dataframe[15])
-
-    parachuteKeys = list(dataframe[19].dropna())
-    parachuteValues = list(dataframe[20].dropna())
-    parachuteSD = list(dataframe[21])
-
-    allValues = []
-    # crating the dictionary
-
-    for i in range(0, len(rocketKeys)):
-
-        if pd.isnull(rocketSD[i]):
-            allValues.append(rocketValues[i])
-        else:
-            allValues.append(((rocketValues[i]), (rocketSD[i])))
-
-    for j in range(0, len(motorKeys)):
-
-        if pd.isnull(motorSD[j]):
-            allValues.append(motorValues[j])
-        else:
-            allValues.append(((motorValues[j]), (motorSD[j])))
-
-    for k in range(0, len(parachuteKeys)):
-
-        if pd.isnull(parachuteSD[k]):
-            allValues.append(parachuteValues[k])
-        else:
-            allValues.append(((parachuteValues[k]), (parachuteSD[k])))
-
-    for l in range(0, len(launchKeys)):
-
-        if pd.isnull(launchSD[l]):
-            allValues.append(launchValues[l])
-        else:
-            allValues.append(((launchValues[l]), (launchSD[l])))
-
-    allKeys = rocketKeys + motorKeys + parachuteKeys + launchKeys
-
-    analysis_parameters = dict(zip(allKeys, allValues))
-    return analysis_parameters
->>>>>>> 05ff0662ddda29e74b6019797ab710725d91831b
