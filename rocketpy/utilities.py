567192714197e2a7488c55af364ae37ff5deb13b
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-09-24 13:20:10 +0200

    ENH: Dealing with expect errors cases

--- rocketpy/utilities.py
+++ rocketpy/utilities.py
@@ -41,8 +41,6 @@ def compute_CdS_from_drop_test(
 
 
 # TODO: Needs tests
-
-
 def calculateEquilibriumAltitude(
     rocket_mass,
     CdS,
@@ -110,7 +108,7 @@ def calculateEquilibriumAltitude(
 
         Parameters
         ----------
-        f : array, list
+        f : array, row
             _description_
         eps : float
             _description_
@@ -199,31 +197,45 @@ def calculateEquilibriumAltitude(
     return altitudeFunction, velocityFunction, final_sol
 
 
-def create_dispersion_dictionary(dic):
-    """creates a dictinary with the rocket data in a excel .csv file.
+# TODO: Needs tests
+def create_dispersion_dictionary(filename):
+    """Creates a dictionary with the rocket data provided by a .csv file.
+    File should be organized in four columns: attribute_class, parameter_name,
+    mean_value, standard_deviation. The first row should be the header.
+    It is advised to use ";" as separator, but "," should work on most of cases.
 
     Parameters
     ----------
-    dic : string
+    filename : string
         String with the path to the .csv file.
 
     Returns
     -------
     dictionary
-        Dictionary with all rocket data used in dispersion analysis.
+        Dictionary with all rocket data to be used in dispersion analysis.
     """
     try:
-        file = np.genfromtxt(dic, usecols=(1, 2, 3), delimiter=",", dtype=str)
+        file = np.genfromtxt(
+            filename, usecols=(1, 2, 3), skip_header=1, delimiter=";", dtype=str
+        )
     except:
-        file = np.genfromtxt(dic, usecols=(1, 2, 3), delimiter=";", dtype=str)
+        print(
+            "Error: The delimiter should be ';'. Using ',' instead, be aware that some resources might not work as expected. Please consider changing the delimiter to ';'."
+        )
+        file = np.genfromtxt(
+            filename, usecols=(1, 2, 3), skip_header=1, delimiter=",", dtype=str
+        )
     analysis_parameters = dict()
-    for list in file:
-        if list[0] != "":
-            if list[2] == "":
+    for row in file:
+        if row[0] != "":
+            if row[2] == "":
                 try:
-                    analysis_parameters[list[0]] = float(list[1])
+                    analysis_parameters[row[0]] = float(row[1])
                 except:
-                    analysis_parameters[list[0]] = eval(list[1])
+                    analysis_parameters[row[0]] = eval(row[1])
             else:
-                analysis_parameters[list[0]] = (float(list[1]), float(list[2]))
+                try:
+                    analysis_parameters[row[0]] = (float(row[1]), float(row[2]))
+                except:
+                    analysis_parameters[row[0]] = ""
     return analysis_parameters
