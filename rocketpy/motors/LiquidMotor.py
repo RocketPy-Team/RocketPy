689eee279646ecaf937980f117766d318c1d5ea3
MrGribel <joao.gribel@hotmail.com>
2022-10-06 11:51:35 -0300

    ENH: add mass flow rate based tank and inertia tensor method

--- rocketpy/motors/LiquidMotor.py
+++ rocketpy/motors/LiquidMotor.py
@@ -152,7 +152,19 @@ class Tank(ABC):
         Function
             Inertia tensor of the tank's fluids as a function of time.
         """
-        ...
+
+        liquid_volume = self.liquidVolume(t)
+        self.cylinder.filled_volume = liquid_volume - self.cap.volume
+
+        cylinder_mass = self.cylinder.filled_volume * self.liquid.density
+
+        # for a solid cylinder, ixx = iyy = mr²/4 + ml²/12
+        self.inertiaI = cylinder_mass * (self.diameter**2 + self.cylinder.filled_height**2 / 12)
+
+        # fluids considered inviscid so no shear resistance from torques in z axis
+        self.inertiaZ = 0
+
+        return [self.inertiaI, self.inertiaZ]
 
 
 # @MrGribel
@@ -173,6 +185,113 @@ class MassFlowRateBasedTank(Tank):
         gas,
     ):
         super().__init__(name, diameter, height, endcap, gas, liquid)
+
+        self.initial_liquid_mass = initial_liquid_mass
+        self.initial_gas_mass = initial_gas_mass
+
+        self.gas_mass_flow_rate_in = Function(
+            gas_mass_flow_rate_in,
+            "Time (s)",
+            "Inlet Gas Propellant Mass Flow Rate (kg/s)",
+            "linear",
+            "constant",
+        )
+
+        self.gas_mass_flow_rate_out = Function(
+            gas_mass_flow_rate_out,
+            "Time (s)",
+            "Outlet Gas Propellant Mass Flow Rate (kg/s)",
+            "linear",
+            "constant",
+        )
+
+        self.liquid_mass_flow_rate_in = Function(
+            liquid_mass_flow_rate_in,
+            "Time (s)",
+            "Inlet Liquid Propellant Mass Flow Rate (kg/s)",
+            "linear",
+            "constant",
+        )
+
+        self.liquid_mass_flow_rate_out = Function(
+            liquid_mass_flow_rate_out,
+            "Time (s)",
+            "Outlet Liquid Propellant Mass Flow Rate (kg/s)",
+            "linear",
+            "constant",
+        )
+
+    def netMassFlowRate(self):
+        """Returns the net mass flow rate of the tank as a function of time.
+        Net mass flow rate is the mass flow rate exiting the tank minus the
+        mass flow rate entering the tank, including liquids and gases.
+
+        Parameters
+        ----------
+        time : float
+            Time in seconds.
+
+        Returns
+        -------
+        Function
+            Net mass flow rate of the tank as a function of time.
+        """
+
+        self.liquid_net_mass_flow_rate = (
+            self.liquid_mass_flow_rate_in - self.liquid_mass_flow_rate_out
+        )
+
+        self.gas_net_mass_flow_rate = (
+            self.gas_mass_flow_rate_in - self.gas_mass_flow_rate_out
+        )
+
+        self.net_mass_flow_rate = (
+            self.liquid_net_mass_flow_rate + self.gas_net_mass_flow_rate
+        )
+
+        return self.net_mass_flow_rate
+
+    def mass(self):
+        """Returns the total mass of liquid and gases inside the tank as a
+        function of time.
+
+        Parameters
+        ----------
+        time : float
+            Time in seconds.
+
+        Returns
+        -------
+        Function
+            Mass of the tank as a function of time. Units in kg.
+        """
+
+        # solve ODE's for liquid and gas masses
+        sol = integrate.solve_ivp(
+            lambda t, y: (
+                self.liquid_net_mass_flow_rate(t),
+                self.gas_net_mass_flow_rate(t),
+            ),
+            (0, self.net_mass_flow_rate.source[:, 0][-1]),
+            (self.initial_liquid_mass, self.initial_gas_mass),
+            vectorized=True,
+        )
+
+        self.liquid_mass = Function(
+            np.column_stack((sol.t, sol.y[0])),
+            "Time (s)",
+            "Liquid Propellant Mass In Tank (kg)",
+        )
+
+        self.gas_mass = Function(
+            np.column_stack((sol.t, sol.y[1])),
+            "Time (s)",
+            "Gas Propellant Mass In Tank (kg)",
+        )
+
+        self.mass = self.liquid_mass + self.gas_mass
+
+        return self.mass
 
 
 # @phmbressan
