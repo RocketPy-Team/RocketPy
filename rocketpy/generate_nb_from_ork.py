cb99c6341c9cc7bcfc7536d8182faf0190394552
patrick <patricksampaio@usp.br>
2022-10-10 10:32:53 +0200

    Enh: Add parachute, fin, tail handling

--- rocketpy/generate_nb_from_ork.py
+++ rocketpy/generate_nb_from_ork.py
@@ -1,4 +1,6 @@
-import xmltodict
+from turtle import distance
+from xml.sax.handler import property_lexical_handler
+import os
 import nbformat
 from bs4 import BeautifulSoup
 import numpy as np
@@ -9,10 +11,20 @@ def generate(ork_file, nb_file, eng_file):
     nb = nbformat.read(nb_file, nbformat.NO_CONVERT)
     bs = BeautifulSoup(open(ork_file).read())
     team_name = ork_file.split("/")[2]
+
+    if os.path.exists('GeneratedSimulations') is False:
+        os.mkdir('GeneratedSimulations')
+
+    if os.path.exists(f'GeneratedSimulations/{team_name}') is False:
+        os.mkdir(f'GeneratedSimulations/{team_name}')
+
+    path = f'GeneratedSimulations/{team_name}/'
+
     with orhelper.OpenRocketInstance() as instance:
         orh = orhelper.Helper(instance)
-        ork = orh.load_doc('./Trajectory Simulations/Team12_OpenRocketProject_v3.04/rocket.ork')
-        
+        ork = orh.load_doc(ork_file)
+        launch_altitude = 160
+
         time_vect = [float(datapoint.text.split(',')[0]) for datapoint in bs.find_all('datapoint')]
         thrust_vect = [float(datapoint.text.split(',')[28]) for datapoint in bs.find_all('datapoint')]
         cg_location_vect = [float(datapoint.text.split(',')[24]) for datapoint in bs.find_all('datapoint')]
@@ -26,91 +38,93 @@ def generate(ork_file, nb_file, eng_file):
         mass = ork.getRocket().getSectionMass()
         elements = process_elements_position(ork.getRocket(), {}, empty_rocket_cm, mass, top_position=0)
 
+        rocket_radius = ork.getRocket().getChild(0).getChild(1).getAftRadius()
+
         normalize_propelant_mass_vect = np.array(propelant_mass_vect)
         normalize_propelant_mass_vect = normalize_propelant_mass_vect - normalize_propelant_mass_vect[np.argmin(normalize_propelant_mass_vect)]
         propelant_mass_vect = list(normalize_propelant_mass_vect)
-
+        propelant_mass = propelant_mass_vect[0]
+        
         #TODO Resolver isso
         longitudinal_moment_of_inertia = [float(datapoint.text.split(',')[21]) for datapoint in bs.find_all('datapoint')]
         rotational_moment_of_inertia = [float(datapoint.text.split(',')[21]) for datapoint in bs.find_all('datapoint')]
 
-        motor_cm = elements['Booser Tube']['DistanceToCG']
-        nozzle_cm = motor_cm - elements['Booser Tube']['length']/2
+        motor_label = bs.find('motormount').parent.find('name').text
+        motor_cm = elements[motor_label]['DistanceToCG']
+        nozzle_cm = motor_cm - elements[motor_label]['length']/2
         # proess burnout from eng_file
         # what to do about the propellent data
         # nozzle radius
         # throuat radius
         burnout_position = np.argwhere(np.array(propelant_mass_vect)==0)[0][0]
         burnout_time = time_vect[burnout_position]
+        tube_length = elements[motor_label]["length"]
 
         # rocket_mass = mass[0] - propelant_mass_vect[0]
-        nb['cells'][15]['source'] = generate_motor_code(team_name, burnout_position, burnout_time, thrust_vect, time_vect)
+        nb['cells'][15]['source'] = generate_motor_code(path, 
+                                                        burnout_position, 
+                                                        burnout_time, 
+                                                        thrust_vect, 
+                                                        time_vect, 
+                                                        propelant_mass, 
+                                                        rocket_radius, 
+                                                        tube_length)
         
 
         drag_coefficient = np.array([time_vect, drag_coefficient]).T
-        
-        rocket_radius = ork.getRocket().getChild(0).getChild(1).getAftRadius()
-        drag_coefficient_source_path = f'{ork_file.split("/")[2]}_drag_coefficient.csv'
+        drag_coefficient_source_path = f'{path}/drag_coefficient.csv'
         np.savetxt(drag_coefficient_source_path, drag_coefficient, delimiter=",")
-        nb['cells'][20]['source'] = f'Calisto = Rocket(\n    motor=Pro75M1670,\n    radius={rocket_radius},\n    mass={mass},\n    inertiaI={longitudinal_moment_of_inertia[0]},\n    inertiaZ=0,\n    distanceRocketNozzle={nozzle_cm},\n    distanceRocketPropellant={motor_cm},\n    powerOffDrag="{drag_coefficient_source_path}",\n    powerOnDrag="{drag_coefficient_source_path}",\n)\n\nCalisto.setRailButtons([0.2, -0.5])'
+        inertiaZ = 0.0351
+        nb['cells'][20]['source'] = f'Calisto = Rocket(\n    motor=Pro75M1670,\n    radius={rocket_radius},\n    mass={mass},\n    inertiaI={longitudinal_moment_of_inertia[0]},\n    inertiaZ={inertiaZ},\n    distanceRocketNozzle={nozzle_cm},\n    distanceRocketPropellant={motor_cm},\n    powerOffDrag="drag_coefficient.csv",\n    powerOnDrag="drag_coefficient.csv",\n)\n\nCalisto.setRailButtons([0.2, -0.5])'
 
         nosecone = bs.find('nosecone')
         nosecone = generate_nosecone_code(bs, empty_rocket_cm)        
 
-        fin = bs.find('trapezoidfinset')
-        n_fin = int(fin.find('fincount').text)
-        root_chord = float(fin.find('rootchord').text)
-        tip_chord = float(fin.find('tipchord').text)
-        span = float(fin.find('sweeplength').text)
-        fin_distance_to_cm = elements["Trapezoidal fin set"]["DistanceToCG"]
-        trapezoidal_fin = f'Calisto.addTrapezoidalFins({n_fin}, rootChord={root_chord}, tipChord={tip_chord}, span={span}, distanceToCM={fin_distance_to_cm}, radius=None, airfoil=None)'
+        trapezoidal_fin = trapezoidal_fin_code(bs, elements)
 
-        tail = bs.find('transition')
-        if tail:
-            topRadius = ork.getRocket().getChild(0).getChild(5).getForeRadius()
-            bottomRadius = ork.getRocket().getChild(0).getChild(5).getAftRadius()
-            tail_length = float(tail.find('length').text)
-            tail = f'Calisto.addTail(\n    topRadius={topRadius}, bottomRadius={bottomRadius}, length={tail_length}, distanceToCM={elements["Boattail"]["DistanceToCG"]}\n)'
-        else:
-            tail = ''
+        tail = tail_code(bs, elements, ork)
         # aerodynamic surfaces
         nb['cells'][23]['source'] = f'{nosecone}\n\n{trapezoidal_fin}\n\n{tail}'
 
-        # main_chute = bs.findAll('parachute')[0]
-        # main_cds = float(main_chute.find('cd').text)
-        # main_position = float(main_chute.find('position').text)
-        # main_deploy_delay = float(main_chute.find('deploydelay').text)
-        # main_deployaltitude = float(main_chute.find('deployaltitude').text)
+        # filter by name = main
+        # sampling rate
+        # trigger functions
+        chute_cell_code = ''
+        chutes = bs.findAll('parachute')
+        
+        for main_chute in filter(lambda x: 'Main' in x.find('name').text, chutes):
+            main_cds = search_cd_chute_if_auto(bs) if main_chute.find('cd').text == 'auto' else float(main_chute.find('cd').text)
+            main_deploy_delay = float(main_chute.find('deploydelay').text)
+            main_deploy_altitude = float(main_chute.find('deployaltitude').text)
 
-        # main_chute = f'Main = Calisto.addParachute(\n    "Main",\n    CdS={main_cds},\n    trigger=mainTrigger,\n    samplingRate=,\n    lag={main_deploy_delay},\n    noise=(0, 8.3, 0.5),\n)'
+            main_code = f'Main = Calisto.addParachute(\n    "Main",\n    CdS={main_cds},\n    trigger=mainTrigger,\n    samplingRate=105,\n    lag={main_deploy_delay},\n    noise=(0, 8.3, 0.5),\n)'
+            main_trigger = f'def mainTrigger(p, y):\n    # p = pressure\n    # y = [x, y, z, vx, vy, vz, e0, e1, e2, e3, w1, w2, w3]\n    # activate main when vz < 0 m/s and z < 800 + 1400 m (+1400 due to surface elevation).\n    return True if y[5] < 0 and y[2] < {main_deploy_altitude} + {launch_altitude} else False'
+            chute_cell_code += f'{main_trigger}\n\n{main_code}\n\n'
 
-        # drogue = bs.findAll('parachute')[0]
-        # drogue_cds = float(drogue.find('cd').text)
-        # drogue_position = float(drogue.find('position').text)
-        # drogue_deploy_delay = float(drogue.find('deploydelay').text)
-        # drogue_deployaltitude = float(drogue.find('deployaltitude').text)
-        # drogue = f'Drogue = Calisto.addParachute(\n    "Drogue",\n    CdS={drogue_cds},\n    trigger=drogueTrigger,\n    samplingRate=,\n    lag={drogue_deploy_delay},\n    noise=(0, 8.3, 0.5),\n)'
+        
+        for drogue in filter(lambda x: 'Drogue' in x.find('name').text, chutes):
+            drogue_cds = search_cd_chute_if_auto(bs) if drogue.find('cd').text == 'auto' else float(drogue.find('cd').text)
+            drogue_deploy_delay = float(drogue.find('deploydelay').text)
+            drogue_code = f'Drogue = Calisto.addParachute(\n    "Drogue",\n    CdS={drogue_cds},\n    trigger=drogueTrigger,\n    samplingRate=105,\n    lag={drogue_deploy_delay},\n    noise=(0, 8.3, 0.5),\n)'
+            drogue_trigger = 'def drogueTrigger(p, y):\n    # p = pressure\n    # y = [x, y, z, vx, vy, vz, e0, e1, e2, e3, w1, w2, w3]\n    # activate drogue when vz < 0 m/s.\n    return True if y[5] < 0 else False\n\n\n'
+            chute_cell_code += f'{drogue_trigger}\n\n{drogue_code}\n'
 
-        # nb['cells'][27]['source'] = f'def drogueTrigger(p, y):\n    # p = pressure\n    # y = [x, y, z, vx, vy, vz, e0, e1, e2, e3, w1, w2, w3]\n    # activate drogue when vz < 0 m/s.\n    return True if y[5] < 0 else False\n\n\ndef mainTrigger(p, y):\n    # p = pressure\n    # y = [x, y, z, vx, vy, vz, e0, e1, e2, e3, w1, w2, w3]\n    # activate main when vz < 0 m/s and z < 800 + 1400 m (+1400 due to surface elevation).\n    return True if y[5] < 0 and y[2] < 800 + 1400 else False\n\n\n{main_chute}\n\n{drogue}'
+        nb['cells'][27]['source'] = chute_cell_code
 
-        nb['cells'][30]['source'] = f'TestFlight = Flight(rocket=Calisto, environment=Env, inclination=85, heading=0)'
+        launch_rod_length = float(bs.find('launchrodlength').text)
+        nb['cells'][7]['source'] = f'Env = Environment(\n    railLength={launch_rod_length}, latitude=39.3897, longitude=-8.28896388889, elevation={launch_altitude}\n)'
 
-    nbformat.write(nb, f'{team_name}.ipynb')
+        launch_heading = float(bs.find('launchrodangle').text)
+        launchrod_direction = min(87, float(bs.find('launchroddirection').text))
+        nb['cells'][30]['source'] = f'TestFlight = Flight(rocket=Calisto, environment=Env, inclination={launchrod_direction}, heading={launch_heading})'
+        print(np.max(altitude_vect))
+
+    nbformat.write(nb, f'{path}Simulation.ipynb')
+
+def search_cd_chute_if_auto(bs):
+    return float(next(filter(lambda x: x.text.replace('.', '').isnumeric(), bs.findAll('cd'))).text)
 
 def calculate_rocket_cg(ork):
-    # mass = ork.getMass()
-    # print(f'{ork.getName()}, mass: {ork.getMass()}, section_mass: {ork.getSectionMass()} component_mass: {ork.getComponentMass()}, cg: {ork.getCG().x}, length: {ork.getLength()}, relative position: {ork.getRelativePosition().toString()}, position: {ork.getPositionValue()}')
-    # has_child = True
-    # i = 0
-    # while has_child:
-    #     try:
-    #         mass += iterator_ork(ork.getChild(i))
-    #         i += 1
-    #     except Exception:
-    #         print(f'{ork.getName()} has {i+1} children')
-    #         has_child = False
-
-    # top_position = 0
     cg_rocket = 0
     top_position = 0
     rocket_mass = ork.getSectionMass()
@@ -120,13 +134,32 @@ def calculate_rocket_cg(ork):
     
     return cg_rocket
 
+after_parent_element = ['After the parent component', 'Bottom of the parent component']
+before_parent_element = ['Top of the parent component']
+
+def calculate_distance_to_cg(ork, rocket_cg, top_position):
+
+    element_position = ork.getRelativePosition().toString()
+    if element_position in after_parent_element:
+        relative_position = top_position + ork.getLength() - ork.getCG().x
+    elif element_position in before_parent_element:
+        relative_position = top_position - ork.getCG().x
+    else:
+        relative_position = top_position + ork.getLength() - ork.getCG().x
+        print(element_position)
+
+    distance_to_cg = rocket_cg - relative_position
+    return distance_to_cg
+    
+
+
 def process_elements_position(ork, elements, rocket_cg, rocket_mass, top_position=0):
     #TODO: Use relative position
 
     element = {
             'length': ork.getLength(),
             'CM': ork.getCG().x,
-            'DistanceToCG': rocket_cg - (top_position + ork.getLength() - ork.getCG().x),
+            'DistanceToCG': calculate_distance_to_cg(ork, rocket_cg, top_position)
         }
     elements[ork.getName()] = element
     has_child = True
@@ -138,23 +171,32 @@ def process_elements_position(ork, elements, rocket_cg, rocket_mass, top_positio
             top_position += ork.getChild(i).getLength()
             i += 1
         except Exception:
-            print(f'{ork.getName()} has {i+1} children')
             has_child = False
-            print(elements)
     return elements
 
+def trapezoidal_fin_code(bs, elements):
+    fin = bs.find('trapezoidfinset')
+    n_fin = int(fin.find('fincount').text)
+    root_chord = float(fin.find('rootchord').text)
+    tip_chord = float(fin.find('tipchord').text)
+    span = float(fin.find('sweeplength').text)
+    fin_label = fin.find('name').text
+    fin_distance_to_cm = elements[fin_label]["DistanceToCG"]
+    trapezoidal_fin = f'Calisto.addTrapezoidalFins({n_fin}, rootChord={root_chord}, tipChord={tip_chord}, span={span}, distanceToCM={fin_distance_to_cm}, radius=None, airfoil=None)'        
+    return trapezoidal_fin
 
-# def find_element_cg():
-
-
-# def find_element_position(rocket, parent_element_position):
-#     position = 0
-#     for idx, parent_section in enumerate(rocket.getChild(0).getChildren()):
-#         if idx ==  parent_element_position:
-#             return position
-#         position =+ parent_section.getLength()
-#     return position
-
+def tail_code(bs, elements, ork):
+    tail = bs.find('transition')
+    if tail:
+        tail_label = tail.find('name').text
+        tail_ork = [ele for ele in ork.getRocket().getChild(0).getChildren()][-1]
+        topRadius = tail_ork.getForeRadius()
+        bottomRadius = tail_ork.getAftRadius()
+        tail_length = float(tail.find('length').text)
+        tail = f'Calisto.addTail(\n    topRadius={topRadius}, bottomRadius={bottomRadius}, length={tail_length}, distanceToCM={elements[tail_label]["DistanceToCG"]}\n)'
+    else:
+        tail = ''    
+    return tail
 
 def generate_nosecone_code(bs, empty_rocket_cm):
     nosecone = bs.find('nosecone')
@@ -165,9 +207,13 @@ def generate_nosecone_code(bs, empty_rocket_cm):
     nosecone = f'NoseCone = Calisto.addNose(length={nosecone_length}, kind="{nosecone_shape}", distanceToCM={nosecone_distanceToCM})'
     return nosecone
 
-def generate_motor_code(name, burnout_position, burnout_time, thrust_vect, time_vect):
+def generate_motor_code(path, burnout_position, burnout_time, thrust_vect, time_vect, propelant_mass, rocket_radius, rocket_height):
+    grain_density = 1815 * 2
+    grain_volume = propelant_mass / grain_density 
+    inner_radius = np.sqrt(- grain_volume / rocket_height + rocket_radius**2)
+    
     thrust_vect = np.array([time_vect[0: burnout_position], thrust_vect[0: burnout_position]]).T
-    thrust_source_name = f'{name}_thrust_source.csv'
+    thrust_source_name = f'{path}/thrust_source.csv'
     np.savetxt(thrust_source_name, thrust_vect, delimiter=",")
-    code = f'Pro75M1670 = SolidMotor(\n    thrustSource="{thrust_source_name}",\n    burnOut={burnout_time},\n    grainNumber=5,\n    grainSeparation=5/1000,\n    grainDensity=1815,\n    grainOuterRadius=33/1000,\n    grainInitialInnerRadius=15/1000,\n    grainInitialHeight=120/1000,\n    nozzleRadius=0,\n    throatRadius=0,\n    interpolationMethod="linear",\n)'
+    code = f'Pro75M1670 = SolidMotor(\n    thrustSource="thrust_source.csv",\n    burnOut={burnout_time},\n    grainNumber=1,\n    grainSeparation=0,\n    grainDensity=1815,\n    grainOuterRadius={rocket_radius},\n    grainInitialInnerRadius={inner_radius},\n    grainInitialHeight={rocket_height},\n    nozzleRadius=0,\n    throatRadius=0,\n    interpolationMethod="linear",\n)'
     return code
