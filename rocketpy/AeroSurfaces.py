ce35ddcde55adb6cdcfcff3a5faebe85522e3881
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-10-24 01:09:21 +0200

    MAINT: adapting attributes names at AeroSurfaces

--- rocketpy/AeroSurfaces.py
+++ rocketpy/AeroSurfaces.py
@@ -153,18 +153,18 @@ class TrapezoidalFins:
         """
         # Store values
         self.numberOfFins = n
-        self.finRadius = radius
-        self.finAirfoil = airfoil
-        self.finDistanceToCM = distanceToCM
-        self.finCantAngle = cantAngle
-        self.finRootChord = rootChord
-        self.finTipChord = tipChord
-        self.finSpan = span
+        self.radius = radius
+        self.airfoil = airfoil
+        self.distanceToCM = distanceToCM
+        self.cantAngle = cantAngle
+        self.rootChord = rootChord
+        self.tipChord = tipChord
+        self.span = span
         self.name = name
 
         # get some nicknames
-        Cr, Ct = self.finRootChord, self.finTipChord
-        s = self.finSpan
+        Cr, Ct = self.rootChord, self.tipChord
+        s = self.span
         cantAngleRad = np.radians(cantAngle)
 
         # Compute auxiliary geometrical parameters
@@ -608,10 +608,10 @@ class Tail:
         """
 
         # Store arguments as attributes
-        self.tailTopRadius = topRadius
-        self.tailBottomRadius = bottomRadius
-        self.tailLength = length
-        self.tailDistanceToCM = distanceToCM
+        self.topRadius = topRadius
+        self.bottomRadius = bottomRadius
+        self.length = length
+        self.distanceToCM = distanceToCM
         self.name = name
         self.radius = radius
 
