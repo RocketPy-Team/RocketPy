a10680c4e9e5f9546476fe2a0abaed64e932e871
MateusStano <mateusstano@usp.br>
2022-11-13 11:52:19 -0300

    bug: fixed __init__ again

--- rocketpy/plots/__init__.py
+++ rocketpy/plots/__init__.py
@@ -1,1 +1,4 @@
-from . import plots
+from .flight_plots import _FlightPlots
+from .environment_plots import _EnvironmentPlots
+from .rocket_plots import _RocketPlots
+from .compare_plots import _ComparePlots
