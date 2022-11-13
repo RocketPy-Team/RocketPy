b81e30f8d9c22b7f66ea5c544b6f27364525ffbe
Giovani Hidalgo Ceotto <ghceotto@gmail.com>
2022-11-12 23:09:03 -0300

    ENH: Prevents cached_property decorator from being used before `__set_name__`

--- rocketpy/tools.py
+++ rocketpy/tools.py
@@ -13,6 +13,9 @@ class cached_property:
     def __get__(self, instance, owner=None):
         if instance is None:
             return self
+        if self.attrname is None:
+            raise TypeError(
+                "Cannot use cached_property instance without calling __set_name__ on it.")
         cache = instance.__dict__
         val = cache.get(self.attrname, _NOT_FOUND)
         if val is _NOT_FOUND:
