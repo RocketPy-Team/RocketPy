48fb4a16290d127f0b821b2e34b2a6dfcad2c3e1
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-14 07:29:05 +0100

    ENH: update the  __init__ method of CompareFlights

--- rocketpy/plots/compare/compare_flights.py
+++ rocketpy/plots/compare/compare_flights.py
@@ -1,7 +1,41 @@
-__author__ = " "
+__author__ = "Guilherme Fernandes Alves, Mateus Stano Junqueira"
 __copyright__ = "Copyright 20XX, RocketPy Team"
 __license__ = "MIT"
 
+import matplotlib.pyplot as plt
+
+
+class CompareFlights:
+    """A class to compare the results of multiple flights.
+
+    Parameters
+    ----------
+    flights : list
+        A list of Flight objects to be compared.
+
+    Attributes
+    ----------
+    flights : list
+        A list of Flight objects to be compared.
+
+    """
+
+    def __init__(self, flights: list) -> None:
+        """Initializes the CompareFlights class.
+
+        Parameters
+        ----------
+        flights : list
+            A list of Flight objects to be compared.
+
+        Returns
+        -------
+        None
+        """
+
+        self.flights = flights
+
+        return None
 
 class _ComparePlots:
     def __init__(self) -> None:
