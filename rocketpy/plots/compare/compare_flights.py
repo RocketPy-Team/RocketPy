1864f14f136c6665f5e57c97928fdfa14c1ceebe
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-14 07:29:41 +0100

    ENH: auxiliary function to create figures and axes

--- rocketpy/plots/compare/compare_flights.py
+++ rocketpy/plots/compare/compare_flights.py
@@ -37,6 +37,99 @@ class CompareFlights:
 
         return None
 
-class _ComparePlots:
-    def __init__(self) -> None:
-        pass
+    def __create_comparison_figure(
+        self,
+        figsize=(7, 10),  # (width, height)
+        legend=True,
+        n_rows=3,
+        n_cols=1,
+        n_plots=3,
+        title="Comparison",
+        x_labels=["Time (s)", "Time (s)", "Time (s)"],
+        y_labels=["x (m)", "y (m)", "z (m)"],
+        flight_attributes=["x", "y", "z"],
+    ):
+        """Creates a figure to compare the results of multiple flights.
+
+        Parameters
+        ----------
+        figsize : tuple, optional
+            The size of the figure, by default (7, 10)
+        legend : bool, optional
+            Whether to show the legend or not, by default True
+        n_rows : int, optional
+            The number of rows of the figure, by default 3
+        n_cols : int, optional
+            The number of columns of the figure, by default 1
+        n_plots : int, optional
+            The number of plots in the figure, by default 3
+        title : str, optional
+            The title of the figure, by default "Comparison"
+        x_labels : list, optional
+            The x labels of each subplot, by default ["Time (s)", "Time (s)", "Time (s)"]
+        y_labels : list, optional
+            The y labels of each subplot, by default ["x (m)", "y (m)", "z (m)"]
+        flight_attributes : list, optional
+            The attributes of the Flight class to be plotted, by default ["x", "y", "z"].
+            The attributes must be a list of strings. Each string must be a valid
+            attribute of the Flight class, i.e., should point to a attribute of
+            the Flight class that is a Function object or a numpy array.
+
+        Returns
+        -------
+        fig : matplotlib.figure.Figure
+            The figure object.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            The axes object.
+        """
+
+        # Create the matplotlib figure
+        fig = plt.figure(figsize=figsize)
+        fig.suptitle(title, fontsize=16, y=1.02, x=0.5)
+
+        # Create the subplots
+        ax = []
+        for i in range(n_plots):
+            ax.append(plt.subplot(n_rows, n_cols, i + 1))
+
+        # Get the maximum time of all the flights
+        max_time = 0
+
+        # Adding the plots to each subplot
+        for flight in self.flights:
+            for i in range(n_plots):
+                try:
+                    ax[i].plot(
+                        flight.time,
+                        flight.__getattribute__(flight_attributes[i])[:, 1],
+                        label=flight.name,
+                    )
+                    # Update the maximum time
+                    max_time = flight.tFinal if flight.time[-1] > max_time else max_time
+                except AttributeError:
+                    raise AttributeError(
+                        f"Invalid attribute {flight_attributes[i]} for the Flight class."
+                    )
+
+        # Set the labels for the x and y axis
+        for i, subplot in enumerate(ax):
+            subplot.set_xlabel(x_labels[i])
+            subplot.set_ylabel(y_labels[i])
+
+        # Set the limits for the x axis
+        for subplot in ax:
+            subplot.set_xlim(0, max_time)
+
+        # Set the legend
+        if legend:
+            fig.legend(
+                loc="upper center",
+                fancybox=True,
+                shadow=True,
+                fontsize=10,
+                bbox_to_anchor=(0.5, 0.995),
+            )
+
+        fig.tight_layout()
+
+        return fig, ax
