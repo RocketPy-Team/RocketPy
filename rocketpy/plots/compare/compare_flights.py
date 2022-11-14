1b17914eade6141ed1cee4362d855b2771d0fa30
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-14 07:35:31 +0100

    ENH: compare attitude frequency response

--- rocketpy/plots/compare/compare_flights.py
+++ rocketpy/plots/compare/compare_flights.py
@@ -877,3 +877,59 @@ class CompareFlights:
             plt.close()
 
         return None
+
+    def attitude_frequency(self, figsize=(7, 10 * 4 / 3), legend=True, filename=None):
+        """Plots the frequency of the attitude of the rocket for the different flights.
+
+        Parameters
+        ----------
+        figsize : tuple, optional
+            standard matplotlib figsize to be used in the plots, by default (7, 10),
+            where the tuple means (width, height).
+        legend : bool, optional
+            Weather or not to show the legend, by default True
+        filename : str, optional
+            If a filename is provided, the plot will be saved to a file, by default None.
+
+        Returns
+        -------
+        None
+        """
+
+        # Create the figure
+        fig, _ = self.__create_comparison_figure(
+            figsize=figsize,
+            legend=legend,
+            n_rows=4,
+            n_cols=1,
+            n_plots=4,
+            title="Comparison of the attitude frequency of the flights",
+            x_labels=[
+                "Frequency (Hz)",
+                "Frequency (Hz)",
+                "Frequency (Hz)",
+                "Frequency (Hz)",
+            ],
+            y_labels=[
+                "Attitude Angle Fourier Amplitude",
+                "Omega 1 Angle Fourier Amplitude",
+                "Omega 2 Angle Fourier Amplitude",
+                "Omega 3 Angle Fourier Amplitude",
+            ],
+            flight_attributes=[
+                "attitudeFrequencyResponse",
+                "omega1FrequencyResponse",
+                "omega2FrequencyResponse",
+                "omega3FrequencyResponse",
+            ],
+        )
+
+        # Saving the plot to a file if a filename is provided, showing the plot otherwise
+        if filename:
+            fig.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
+            plt.close()
+
+        return None
