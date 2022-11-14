e2de760a7e7076d3bdb897ee4fcc98ce8ec14e7b
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-14 07:31:39 +0100

    ENH: compare euler angles method

--- rocketpy/plots/compare/compare_flights.py
+++ rocketpy/plots/compare/compare_flights.py
@@ -321,3 +321,51 @@ class CompareFlights:
             plt.close()
 
         return None
+
+    def euler_angles(self, figsize=(7, 10), legend=True, filename=None):
+        """Plots a comparison of the euler angles of the rocket for the different
+        flights.
+
+        Parameters
+        ----------
+        figsize : tuple, optional
+            standard matplotlib figsize to be used in the plots, by default (7, 10),
+            where the tuple means (width, height).
+        legend : bool, optional
+            Weather or not to show the legend, by default True
+        filename : str, optional
+            If a filename is provided, the plot will be saved to a file, by default None.
+            Image options are: png, pdf, ps, eps and svg.
+
+        Returns
+        -------
+        None
+        """
+
+        # Create the figure
+        fig, _ = self.__create_comparison_figure(
+            figsize=figsize,
+            legend=legend,
+            n_rows=3,
+            n_cols=1,
+            n_plots=3,
+            title="Comparison of the euler angles of the flights",
+            x_labels=["Time (s)", "Time (s)", "Time (s)"],
+            y_labels=[
+                self.flights[0].phi.getOutputs()[0],
+                self.flights[0].theta.getOutputs()[0],
+                self.flights[0].psi.getOutputs()[0],
+            ],
+            flight_attributes=["phi", "theta", "psi"],
+        )
+
+        # Saving the plot to a file if a filename is provided, showing the plot otherwise
+        if filename:
+            fig.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
+            plt.close()
+
+        return None
+
