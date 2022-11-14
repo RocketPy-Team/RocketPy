e55c5f22f1706237149efec1658874e9aeb4004f
Gui-FernandesBR <guilherme_fernandes@usp.br>
2022-11-14 07:31:20 +0100

    ENH: compare accelerations method

--- rocketpy/plots/compare/compare_flights.py
+++ rocketpy/plots/compare/compare_flights.py
@@ -273,3 +273,51 @@ class CompareFlights:
             plt.close()
 
         return None
+
+    def accelerations(self, figsize=(7, 10 * 4 / 3), legend=True, filename=None):
+        """Plots a comparison of the acceleration of the rocket in the three
+        dimensions separately.
+
+        Parameters
+        ----------
+        figsize : tuple, optional
+            standard matplotlib figsize to be used in the plots, by default (7, 10),
+            where the tuple means (width, height).
+        legend : bool, optional
+            Weather or not to show the legend, by default True
+        filename : str, optional
+            If a filename is provided, the plot will be saved to a file, by default None.
+            Image options are: png, pdf, ps, eps and svg.
+
+        Returns
+        -------
+        None
+        """
+
+        # Create the figure
+        fig, _ = self.__create_comparison_figure(
+            figsize=figsize,
+            legend=legend,
+            n_rows=4,
+            n_cols=1,
+            n_plots=4,
+            title="Comparison of the acceleration of the flights",
+            x_labels=["Time (s)", "Time (s)", "Time (s)", "Time (s)"],
+            y_labels=[
+                "Acceleration (m/s^2)",
+                "ax (m/s^2)",
+                "ay (m/s^2)",
+                "az (m/s^2)",
+            ],
+            flight_attributes=["acceleration", "ax", "ay", "az"],
+        )
+
+        # Saving the plot to a file if a filename is provided, showing the plot otherwise
+        if filename:
+            fig.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
+            plt.close()
+
+        return None
