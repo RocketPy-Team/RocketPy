e5a91c76ca44713b8ffcf742c1b7a3cd23e6a271
Giovani Hidalgo Ceotto <ghceotto@gmail.com>
2022-11-16 02:22:05 -0300

    ENH: Create EmptyMotor class

--- rocketpy/Motor.py
+++ rocketpy/Motor.py
@@ -1724,3 +1724,24 @@ class HybridMotor(Motor):
 
     def allInfo(self):
         pass
+
+
+class EmptyMotor:
+    """Class that represents an empty motor with no mass and no thrust."""
+
+    # TODO: This is a temporary solution. It should be replaced by a class that
+    # inherits from the abstract Motor class. Currently cannot be done easily.
+    def __init__(self):
+        """Initializes an empty motor with no mass and no thrust."""
+        self._csys = 1
+        self.nozzleRadius = 0
+        self.thrust = Function(0, "Time (s)", "Thrust (N)")
+        self.mass = Function(0, "Time (s)", "Mass (kg)")
+        self.massDot = Function(0, "Time (s)", "Mass Depletion Rate (kg/s)")
+        self.burnOutTime = 1
+        self.nozzlePosition = 0
+        self.centerOfMass = Function(0, "Time (s)", "Mass (kg)")
+        self.inertiaZ = Function(0, "Time (s)", "Moment of Inertia Z (kg m²)")
+        self.inertiaI = Function(0, "Time (s)", "Moment of Inertia I (kg m²)")
+        self.inertiaZDot = Function(0, "Time (s)", "Propellant Inertia Z Dot (kgm²/s)")
+        self.inertiaIDot = Function(0, "Time (s)", "Propellant Inertia I Dot (kgm²/s)")
