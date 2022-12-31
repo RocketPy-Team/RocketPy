49d4b75a13c652875b7019ba0aac5440e017349d
Guilherme <guilherme_fernandes@usp.br>
2022-12-31 08:33:00 +0100

    MAINT: improve docstrings for tests

--- tests/test_environment_anaysis.py
+++ tests/test_environment_anaysis.py
@@ -12,55 +12,26 @@ from rocketpy import EnvironmentAnalysis
 
 plt.rcParams.update({"figure.max_open_warning": 0})
 
-# Create a simple object of the Environment Analysis class
-@pytest.fixture
-def env():
-    """Create a simple object of the Environment Analysis class to be used in
-    the tests. This allows to avoid repeating the same code in all tests.
 
-    Returns
-    -------
-    EnvironmentAnalysis
-        A simple object of the Environment Analysis class
-    """
-    env = EnvironmentAnalysis(
-        start_date=datetime.datetime(2019, 10, 23),
-        end_date=datetime.datetime(2021, 10, 23),
-        latitude=39.3897,
-        longitude=-8.28896388889,
-        start_hour=6,
-        end_hour=18,
-        surfaceDataFile="./data/weather/EuroC_single_level_reanalysis_2002_2021.nc",
-        pressureLevelDataFile="./data/weather/EuroC_pressure_levels_reanalysis_2001-2021.nc",
-        timezone=None,
-        unit_system="metric",
-        forecast_date=None,
-        forecast_args=None,
-        maxExpectedAltitude=None,
-    )
-
-    return env
-
-
-def test_allInfo(env):
+def test_allInfo(env_analysis):
     """Test the EnvironmentAnalysis.allInfo() method, which already invokes
     several other methods. It is a good way to test the whole class in a first view.
     However, if it fails, it is hard to know which method is failing.
 
     Parameters
     ----------
-    env : EnvironmentAnalysis
+    env_analysis : EnvironmentAnalysis
         A simple object of the Environment Analysis class
 
     Returns
     -------
     None
     """
-    assert env.allInfo() == None
+    assert env_analysis.allInfo() == None
 
 
 @patch("matplotlib.pyplot.show")
-def test_distribution_plots(mock_show, env):
+def test_distribution_plots(mock_show, env_analysis):
     """Tests the distribution plots method of the EnvironmentAnalysis class. It
     only checks if the method runs without errors. It does not check if the
     plots are correct, as this would require a lot of work and would be
@@ -68,7 +39,7 @@ def test_distribution_plots(mock_show, env):
 
     Parameters
     ----------
-    env : EnvironmentAnalysis
+    env_analysis : EnvironmentAnalysis
         A simple object of the EnvironmentAnalysis class.
 
     Returns
@@ -77,14 +48,17 @@ def test_distribution_plots(mock_show, env):
     """
 
     # Check distribution plots
-    assert env.plot_wind_gust_distribution() == None
-    assert env.plot_surface10m_wind_speed_distribution() == None
-    assert env.plot_wind_gust_distribution_over_average_day() == None
-    assert env.plot_sustained_surface_wind_speed_distribution_over_average_day() == None
+    assert env_analysis.plot_wind_gust_distribution() == None
+    assert env_analysis.plot_surface10m_wind_speed_distribution() == None
+    assert env_analysis.plot_wind_gust_distribution_over_average_day() == None
+    assert (
+        env_analysis.plot_sustained_surface_wind_speed_distribution_over_average_day()
+        == None
+    )
 
 
 @patch("matplotlib.pyplot.show")
-def test_average_plots(mock_show, env):
+def test_average_plots(mock_show, env_analysis):
     """Tests the average plots method of the EnvironmentAnalysis class. It
     only checks if the method runs without errors. It does not check if the
     plots are correct, as this would require a lot of work and would be
@@ -92,7 +66,7 @@ def test_average_plots(mock_show, env):
 
     Parameters
     ----------
-    env : EnvironmentAnalysis
+    env_analysis : EnvironmentAnalysis
         A simple object of the EnvironmentAnalysis class.
 
     Returns
@@ -100,48 +74,94 @@ def test_average_plots(mock_show, env):
     None
     """
     # Check "average" plots
-    assert env.plot_average_temperature_along_day() == None
-    assert env.plot_average_surface10m_wind_speed_along_day(False) == None
-    assert env.plot_average_surface10m_wind_speed_along_day(True) == None
-    assert env.plot_average_sustained_surface100m_wind_speed_along_day() == None
-    assert env.plot_average_day_wind_rose_all_hours() == None
-    assert env.plot_average_day_wind_rose_specific_hour(12) == None
-    assert env.plot_average_day_wind_rose_specific_hour(12) == None
+    assert env_analysis.plot_average_temperature_along_day() == None
+    assert env_analysis.plot_average_surface10m_wind_speed_along_day(False) == None
+    assert env_analysis.plot_average_surface10m_wind_speed_along_day(True) == None
+    assert (
+        env_analysis.plot_average_sustained_surface100m_wind_speed_along_day() == None
+    )
+    assert env_analysis.plot_average_day_wind_rose_all_hours() == None
+    assert env_analysis.plot_average_day_wind_rose_specific_hour(12) == None
+    assert env_analysis.plot_average_day_wind_rose_specific_hour(12) == None
 
 
 @patch("matplotlib.pyplot.show")
-def test_profile_plots(mock_show, env):
+def test_profile_plots(mock_show, env_analysis):
+    """Check the profile plots method of the EnvironmentAnalysis class. It
+    only checks if the method runs without errors. It does not check if the
+    plots are correct, as this would require a lot of work and would be
+    difficult to maintain.
+
+    Parameters
+    ----------
+    mock_show : Mock
+        Mock of the matplotlib.pyplot.show() method
+    env_analysis : EnvironmentAnalysis
+        A simple object of the EnvironmentAnalysis class.
+    """
     # Check profile plots
-    assert env.plot_wind_heading_profile_over_average_day() == None
-    assert env.plot_average_wind_heading_profile(clear_range_limits=False) == None
-    assert env.plot_average_wind_heading_profile(clear_range_limits=True) == None
-    assert env.plot_average_wind_speed_profile(clear_range_limits=False) == None
-    assert env.plot_average_wind_speed_profile(clear_range_limits=True) == None
-    assert env.plot_average_pressure_profile(clear_range_limits=False) == None
-    assert env.plot_average_pressure_profile(clear_range_limits=True) == None
-    assert env.plot_wind_profile_over_average_day() == None
+    assert env_analysis.plot_wind_heading_profile_over_average_day() == None
+    assert (
+        env_analysis.plot_average_wind_heading_profile(clear_range_limits=False) == None
+    )
+    assert (
+        env_analysis.plot_average_wind_heading_profile(clear_range_limits=True) == None
+    )
+    assert (
+        env_analysis.plot_average_wind_speed_profile(clear_range_limits=False) == None
+    )
+    assert env_analysis.plot_average_wind_speed_profile(clear_range_limits=True) == None
+    assert env_analysis.plot_average_pressure_profile(clear_range_limits=False) == None
+    assert env_analysis.plot_average_pressure_profile(clear_range_limits=True) == None
+    assert env_analysis.plot_wind_profile_over_average_day() == None
 
 
 @patch("matplotlib.pyplot.show")
-def test_animation_plots(mock_show, env):
+def test_animation_plots(mock_show, env_analysis):
+    """Check the animation plots method of the EnvironmentAnalysis class. It
+    only checks if the method runs without errors. It does not check if the
+    plots are correct, as this would require a lot of work and would be
+    difficult to maintain.
+
+    Parameters
+    ----------
+    mock_show : Mock
+        Mock of the matplotlib.pyplot.show() method
+    env_analysis : EnvironmentAnalysis
+        A simple object of the EnvironmentAnalysis class.
+    """
 
     # Check animation plots
-    assert isinstance(env.animate_average_wind_rose(), widgets.Image)
-    assert isinstance(env.animate_wind_gust_distribution_over_average_day(), HTML)
-    assert isinstance(env.animate_wind_heading_profile_over_average_day(), HTML)
-    assert isinstance(env.animate_wind_profile_over_average_day(), HTML)
+    assert isinstance(env_analysis.animate_average_wind_rose(), widgets.Image)
     assert isinstance(
-        env.animate_sustained_surface_wind_speed_distribution_over_average_day(),
+        env_analysis.animate_wind_gust_distribution_over_average_day(), HTML
+    )
+    assert isinstance(
+        env_analysis.animate_wind_heading_profile_over_average_day(), HTML
+    )
+    assert isinstance(env_analysis.animate_wind_profile_over_average_day(), HTML)
+    assert isinstance(
+        env_analysis.animate_sustained_surface_wind_speed_distribution_over_average_day(),
         HTML,
     )
 
 
-def test_exports(env):
+def test_exports(env_analysis):
+    """Check the export methods of the EnvironmentAnalysis class. It
+    only checks if the method runs without errors. It does not check if the
+    files are correct, as this would require a lot of work and would be
+    difficult to maintain.
 
-    assert env.exportMeanProfiles() == None
-    assert env.save("EnvAnalysisDict") == None
+    Parameters
+    ----------
+    env_analysis : EnvironmentAnalysis
+        A simple object of the EnvironmentAnalysis class.
+    """
 
-    env2 = copy.deepcopy(env)
+    assert env_analysis.exportMeanProfiles() == None
+    assert env_analysis.save("EnvAnalysisDict") == None
+
+    env2 = copy.deepcopy(env_analysis)
     env2.load("EnvAnalysisDict")
     assert env2.allInfo() == None
 
--- tests/test_function.py
+++ tests/test_function.py
@@ -9,37 +9,16 @@ from rocketpy import Function
 plt.rcParams.update({"figure.max_open_warning": 0})
 
 
-@pytest.fixture
-def linear_func():
-    """Create a linear function based on a list of points. The function
-    represents y = x and may be used on different tests.
-
-    Returns
-    -------
-    Function
-        A linear function representing y = x.
-    """
-    return Function(
-        [[0, 0], [1, 1], [2, 2], [3, 3]],
-    )
-
-
-@pytest.fixture
-def func_from_csv():
-    func = Function(
-        source="tests/fixtures/airfoils/e473-10e6-degrees.csv",
-        inputs=["Scalar"],
-        outputs=["Scalar"],
-        interpolation="linear",
-        extrapolation="linear",
-    )
-    return func
-
-
 # Test Function creation from .csv file
 def test_function_from_csv(func_from_csv):
-    func = func_from_csv
-    # Assert func is zero at 0 but with tolerance
+    """Test the Function class creation from a .csv file.
+
+    Parameters
+    ----------
+    func_from_csv : rocketpy.Function
+        A Function object created from a .csv file.
+    """
+    # Assert the function is zero at 0 but with a certain tolerance
     assert np.isclose(func_from_csv(0), 0.0, atol=1e-6)
     # Check the __str__ method
     assert func_from_csv.__str__() == "Function from R1 to R1 : (Scalar) → (Scalar)"
@@ -48,6 +27,13 @@ def test_function_from_csv(func_from_csv):
 
 
 def test_getters(func_from_csv):
+    """Test the different getters of the Function class.
+
+    Parameters
+    ----------
+    func_from_csv : rocketpy.Function
+        A Function object created from a .csv file.
+    """
     assert func_from_csv.getInputs() == ["Scalar"]
     assert func_from_csv.getOutputs() == ["Scalar"]
     assert func_from_csv.getInterpolationMethod() == "linear"
@@ -59,6 +45,13 @@ def test_getters(func_from_csv):
 
 
 def test_setters(func_from_csv):
+    """Test the different setters of the Function class.
+
+    Parameters
+    ----------
+    func_from_csv : rocketpy.Function
+        A Function object created from a .csv file.
+    """
     # Test set methods
     func_from_csv.setInputs(["Scalar2"])
     assert func_from_csv.getInputs() == ["Scalar2"]
@@ -72,6 +65,15 @@ def test_setters(func_from_csv):
 
 @patch("matplotlib.pyplot.show")
 def test_plots(mock_show, func_from_csv):
+    """Test different plot methods of the Function class.
+
+    Parameters
+    ----------
+    mock_show : Mock
+        Mock of the matplotlib.pyplot.show method.
+    func_from_csv : rocketpy.Function
+        A Function object created from a .csv file.
+    """
     # Test plot methods
     assert func_from_csv.plot() == None
     # Test comparePlots
@@ -88,7 +90,14 @@ def test_plots(mock_show, func_from_csv):
 
 
 def test_interpolation_methods(linear_func):
+    """Test some of the interpolation methods of the Function class. Methods
+    not tested here are already being called in other tests.
 
+    Parameters
+    ----------
+    linear_func : rocketpy.Function
+        A Function object created from a list of values.
+    """
     # Test Akima
     linear_func.setInterpolation("akima")
     assert linear_func.getInterpolationMethod() == "akima"
@@ -101,7 +110,14 @@ def test_interpolation_methods(linear_func):
 
 
 def test_extrapolation_methods(linear_func):
+    """Test some of the extrapolation methods of the Function class. Methods
+    not tested here are already being called in other tests.
 
+    Parameters
+    ----------
+    linear_func : rocketpy.Function
+        A Function object created from a list of values.
+    """
     # Test zero
     linear_func.setExtrapolation("zero")
     assert linear_func.getExtrapolationMethod() == "zero"
@@ -119,5 +135,12 @@ def test_extrapolation_methods(linear_func):
 
 
 def test_integral(linear_func):
+    """Test the integral method of the Function class.
+
+    Parameters
+    ----------
+    linear_func : rocketpy.Function
+        A Function object created from a list of values.
+    """
     # Test integral
     assert np.isclose(linear_func.integral(0, 1), 0.5, atol=1e-6)
