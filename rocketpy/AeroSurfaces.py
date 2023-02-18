696d631f0c78fea7cc2b9e1808ef399a0898f7b1
Franz Masatoshi Yuri <franz.yuri@usp.br>
2023-02-18 12:02:55 -0300

    enh:airfoilInfo

--- rocketpy/AeroSurfaces.py
+++ rocketpy/AeroSurfaces.py
@@ -382,13 +382,13 @@ class Fins(ABC):
         else:
             # Defines clalpha2D as the derivative of the
             # lift coefficient curve for a specific airfoil
-            airfoilCl = Function(
+            self.airfoilCl = Function(
                 self.airfoil[0],
                 interpolation="linear",
             )
 
             # Differentiating at x = 0 to get cl_alpha
-            clalpha2D_Mach0 = airfoilCl.differentiate(x=1e-3, dx=1e-3)
+            clalpha2D_Mach0 = self.airfoilCl.differentiate(x=1e-3, dx=1e-3)
 
             # Convert to radians if needed
             if self.airfoil[1] == "degrees":
@@ -633,23 +633,31 @@ class Fins(ABC):
                 self.rollForcingInterferenceFactor
             )
         )
-        # TODO: lacks a title for the plot
+        # lacks a title for the plot
         self.rollParameters[0]()
-        # TODO: lacks a title for the plot
+        # lacks a title for the plot
         self.rollParameters[1]()
 
         return None
 
-    def airfoilPlots(self):
+    def airfoilInfo(self):
+        """Prints out airfoil related information of the
+        fin set.
+
+        Parameters
+        ----------
+        None
+
+        Return
+        ------
+        None
+        """
         if self.airfoil is not None:
-            airfoilCl = Function(
-                self.airfoil[0],
-                inputs="angle of attack",
-                outputs="lift coefficient",
-                interpolation="linear",
-                extrapolation=None,
-            )
-            airfoilCl.plot1D()
+            print("\n\nAerodynamic Information\n")
+            print("Airfoil's Lift Curve as a Function of Alpha ({}))".format(self.airfoil[1]))
+            self.airfoilCl.plot1D()
+        
+        return None
 
     def allInfo(self):
         """Prints out all data and graphs available about the Rocket.
@@ -675,7 +683,7 @@ class Fins(ABC):
         self.geometricalInfo()
         self.aerodynamicInfo()
         self.rollInfo()
-        self.airfoilPlots()
+        self.airfoilInfo()
 
         return None
 
@@ -1479,6 +1487,7 @@ class Tail:
         return None
 
     def aerodynamicInfo(self):
+
         print(f"\nTail name: {self.name}")
         print(f"Tail Center of Pressure Position in Local Coordinates: {self.cp} m")
         print(f"Tail Lift Coefficient Slope: {self.clalpha:.3f} 1/rad")
