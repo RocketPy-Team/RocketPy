c1584be430ad1a50060b349b1b7365c1f019444d
MateusStano <mateusstano@usp.br>
2023-04-09 23:42:14 -0300

    MAINT: parachute docs

--- rocketpy/Parachute.py
+++ rocketpy/Parachute.py
@@ -12,60 +12,56 @@ from .prints.parachute_prints import _ParachutePrints
 class Parachute:
     """Keeps parachute information.
 
-        Attributes
-        ----------
-    Parachute attributes:
-        name : string
-            Parachute name, such as drogue and main. Has no impact in
-            simulation, as it is only used to display data in a more
-            organized matter.
-        CdS : float
-            Drag coefficient times reference area for parachute. It is
-            used to compute the drag force exerted on the parachute by
-            the equation F = ((1/2)*rho*V^2)*CdS, that is, the drag
-            force is the dynamic pressure computed on the parachute
-            times its CdS coefficient. Has units of area and must be
-            given in squared meters.
-        trigger : function
-            Function which defines if the parachute ejection system is
-            to be triggered. It must take as input the freestream
-            pressure in pascal and the state vector of the simulation,
-            which is defined by [x, y, z, vx, vy, vz, e0, e1, e2, e3, wx, wy, wz].
-            It will be called according to the sampling rate given next.
-            It should return True if the parachute ejection system is
-            to be triggered and False otherwise.
-        samplingRate : float
-            Sampling rate in which the trigger function works. It is used to
-            simulate the refresh rate of onboard sensors such as barometers.
-            Default value is 100. Value must be given in hertz.
-        lag : float
-            Time between the parachute ejection system is triggered and the
-            parachute is fully opened. During this time, the simulation will
-            consider the rocket as flying without a parachute. Default value
-            is 0. Must be given in seconds.
-        noiseBias : float
-            Mean value of the noise added to the pressure signal, which is
-            passed to the trigger function. Unit is in pascal.
-        noiseDeviation : float
-            Standard deviation of the noise added to the pressure signal,
-            which is passed to the trigger function. Unit is in pascal.
-        noiseCorr : tuple, list
-            Tuple with the correlation between noise and time.
-        noiseSignal : list
-            List of (t, noise signal) corresponding to signal passed to
-            trigger function. Completed after running a simulation.
-        noisyPressureSignal : list
-            List of (t, noisy pressure signal) that is passed to the
-            trigger function. Completed after running a simulation.
-        cleanPressureSignal : list
-            List of (t, clean pressure signal) corresponding to signal passed to
-            trigger function. Completed after running a simulation.
-        noiseSignalFunction : Function
-            Function of noiseSignal.
-        noisyPressureSignalFunction : Function
-            Function of noisyPressureSignal.
-        cleanPressureSignalFunction : Function
-            Function of cleanPressureSignal.
+    Attributes
+    ----------
+    name : string
+        Parachute name, such as drogue and main. Has no impact in
+        simulation, as it is only used to display data in a more
+        organized matter.
+    CdS : float
+        Drag coefficient times reference area for parachute. It is
+        used to compute the drag force exerted on the parachute by
+        the equation F = ((1/2)*rho*V^2)*CdS, that is, the drag
+        force is the dynamic pressure computed on the parachute
+        times its CdS coefficient. Has units of area and must be
+        given in squared meters.
+    trigger : function
+        Function which defines if the parachute ejection system is
+        to be triggered. It must take as input the freestream
+        pressure in pascal, the state vector of the simulation,
+        which is defined by [x, y, z, vx, vy, vz, e0, e1, e2, e3, wx, wy, wz],
+        and the height in meters (above ground level).
+        It will be called according to the sampling rate given next.
+        It should return True if the parachute ejection system is
+        to be triggered and False otherwise.
+    samplingRate : float
+        Sampling rate, in hertz, for the trigger function.
+    lag : float
+        Time, in seconds, between the parachute ejection system is triggered
+        and the parachute is fully opened.
+    noiseBias : float
+        Mean value of the noise added to the pressure signal, which is
+        passed to the trigger function. Unit is in pascal.
+    noiseDeviation : float
+        Standard deviation of the noise added to the pressure signal,
+        which is passed to the trigger function. Unit is in pascal.
+    noiseCorr : tuple, list
+        Tuple with the correlation between noise and time.
+    noiseSignal : list
+        List of (t, noise signal) corresponding to signal passed to
+        trigger function. Completed after running a simulation.
+    noisyPressureSignal : list
+        List of (t, noisy pressure signal) that is passed to the
+        trigger function. Completed after running a simulation.
+    cleanPressureSignal : list
+        List of (t, clean pressure signal) corresponding to signal passed to
+        trigger function. Completed after running a simulation.
+    noiseSignalFunction : Function
+        Function of noiseSignal.
+    noisyPressureSignalFunction : Function
+        Function of noisyPressureSignal.
+    cleanPressureSignalFunction : Function
+        Function of cleanPressureSignal.
     """
 
     def __init__(
@@ -78,19 +74,33 @@ class Parachute:
         noise=(0, 0, 0),
     ):
         """Initializes Parachute class.
+
         Parameters
         ----------
         name : string
-            Name of the parachute.
+            Parachute name, such as drogue and main. Has no impact in
+            simulation, as it is only used to display data in a more
+            organized matter.
         CdS : float
-            CdS of the parachute.
-        Trigger : function, float, string
-            Trigger function, altitude of ejection or "apogee".
+            Drag coefficient times reference area of the parachute.
+        trigger : function, float, string
+            Function which defines if the parachute ejection system is
+            to be triggered. It must take as input the freestream
+            pressure in pascal and the state vector of the simulation,
+            which is defined by [x, y, z, vx, vy, vz, e0, e1, e2, e3, wx, wy, wz],
+            and the height in meters (above ground level).
+            It will be called according to the sampling rate given next.
+            It should return True if the parachute ejection system is
+            to be triggered and False otherwise.
         samplingRate : float
-            Sampling rate, in hertz, for the Trigger function.
+            Sampling rate in which the parachute trigger will be checked at.
+            It is used to simulate the refresh rate of onboard sensors such
+            as barometers. Default value is 100. Value must be given in hertz.
         lag : float
-            Time, in seconds, between the parachute ejection system is triggered and the
-        parachute is fully opened.
+            Time between the parachute ejection system is triggered and the
+            parachute is fully opened. During this time, the simulation will
+            consider the rocket as flying without a parachute. Default value
+            is 0. Must be given in seconds.
         noise : tuple, list, optional
             List in the format (mean, standard deviation, time-correlation).
             The values are used to add noise to the pressure signal which is
