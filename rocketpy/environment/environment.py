d75e11ccbcf4d24e29849a92c53b20f6cc970af4
Lint Action <lint-action@samuelmeuli.com>
2023-10-09 03:37:43 +0000

    Fix code style issues with Black

--- rocketpy/environment/environment.py
+++ rocketpy/environment/environment.py
@@ -482,7 +482,9 @@ class Environment:
     @max_expected_height.setter
     def max_expected_height(self, value):
         if value < self.elevation:
-            raise ValueError("Max expected height cannot be lower than the surface elevation")
+            raise ValueError(
+                "Max expected height cannot be lower than the surface elevation"
+            )
         self._max_expected_height = value
         self.plots.grid = np.linspace(self.elevation, self.max_expected_height)
 
