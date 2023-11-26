aab536622fa65bd7f8dc22b1777089c17eae5bdd
Gui-FernandesBR <guilherme_fernandes@usp.br>
2023-11-25 22:18:58 -0300

    ENH: Refactor new class to be more flexible

--- rocketpy/simulation/df2flight.py
+++ rocketpy/simulation/df2flight.py
@@ -1,87 +1,34 @@
-"""Starts with a pandas DataFrame containing the rocket's collected flight data
+"""Starts with .csv or .txt file containing the rocket's collected flight data
 and build a rocketpy.Flight object from it.
 """
-
-from dataclasses import dataclass
-from functools import cached_property, wraps
+import warnings
 
 import numpy as np
 
 from rocketpy.mathutils import Function
-from rocketpy.tools import import_optional_dependency
+from rocketpy.units import UNITS_CONVERSION_DICT
 
-pandas = import_optional_dependency("pandas")
-pd = pandas
-
-
-def df2property(
-    col_name, output_name, interpolation="spline", extrapolation="constant"
-):
-    """Decorator to create a property from a data frame. The property works as a
-    rocketpy.Function object. Check the DataFrameToFlight class for additional
-    details
-
-    Parameters
-    ----------
-    col_name : str
-        The name of the column in the data frame. This is case sensitive.
-    output_name : str
-        The label of the output to be used in the rocketpy.Function object.
-    interpolation : str, optional
-        The interpolation method to be used in the rocketpy.Function object.
-        The default is "spline".
-    extrapolation : str, optional
-        The extrapolation method to be used in the rocketpy.Function object.
-        The default is "constant".
-
-    Returns
-    -------
-    decorator
-        The decorator to be used in the DataFrameToFlight class. This only works
-        in the DataFrameToFlight class.
-    """
-
-    class Decorator:
-        """A decorator class that wraps a function and provides create a
-        property from a data frame. This is made via class because it's the only
-        way to use the __set_name__ method. This method is used to get the name
-        of the property in the class.
-        """
-
-        func = None
-
-        def __init__(self, func):
-            self.attrname = None
-            self.func = func
-
-        def __set_name__(self, owner, name):
-            self.attrname = name
-
-        @wraps(func)
-        def __get__(self, instance, owner=None):
-            """Get method that retrieves the value from the data frame and
-            returns a Function object.
-            """
-            try:
-                name = instance.columns_map[col_name]
-            except KeyError as e:
-                raise KeyError(
-                    f"Cannot get the '{col_name}' variable from the data frame."
-                    + " Please check your columns_map."
-                ) from e
-            x_values = instance.df[name].values
-            source = np.column_stack((instance.time, x_values))
-            return Function(
-                source, "Time (s)", output_name, interpolation, extrapolation
-            )
-
-    return Decorator
+FLIGHT_LABEL_MAP = {
+    # "name of Flight Attribute": "Label to be displayed"
+    "time": "Time (s)",
+    "x": "Position x (m)",
+    "y": "Position y (m)",
+    "z": "Position z (m)",
+    "vx": "Velocity x (m/s)",
+    "vy": "Velocity y (m/s)",
+    "vz": "Velocity z (m/s)",
+    "ax": "Acceleration x (m/s^2)",
+    "ay": "Acceleration y (m/s^2)",
+    "az": "Acceleration z (m/s^2)",
+    "altitude": "Altitude AGL (m)",
+    "latitude": "Latitude (deg)",
+    "longitude": "Longitude (deg)",
+}
 
 
-@dataclass
-class DataFrameToFlight:
-    """A class to create a rocketpy.Flight object from a pandas data frame. Each
-    property of a Flight object will be created using values from the data frame
+class FlightDataImporter:
+    """A class to create a rocketpy.Flight object from a .csv file. The data
+    frame must contain a time column in seconds.
 
     There are different ways of calculating a property from the data frame.
         1. The property is already in data frame and you want to use it as is
@@ -95,114 +42,155 @@ class DataFrameToFlight:
     name : str
         The name of the flight. This is useful to identify the flight in the
         simulation. Example: "Rocket Calisto, Spaceport America, 2019"
-    time_col : str
-        The name of the column containing the time values. This is case
-        sensitive. This column is crucial because it is used to create the
-        other rocketpy.Function objects.
-    df : pd.DataFrame
-        The data frame containing the flight data. The data frame must contain
-        the time column.
+    filepath : str, Path
+        The path
     columns_map : dict, optional
         A dictionary containing the mapping between the column names in the
         data frame and the names to be used in the Flight object. This is useful
         when the column names in the data frame are not the same as the ones
         used in the Flight object. The default is None.
+    units : dict, optional
+        A dictionary containing the units of the columns present in the file.
+        This is important when the units in the file are not SI. The default is
+        None, meaning that no unit conversion will be performed. If a dictionary
+        is passed, the columns will be converted to SI units if follows the
+        format: {"column_name1": "unit1", "column_name2": "unit2", ...}.
     interpolation : str, optional
         The interpolation method to be used in the rocketpy.Function objects.
         The default is "linear".
     extrapolation : str, optional
         The extrapolation method to be used in the rocketpy.Function objects.
         The default is "zero".
+    separator : str, optional
+        The separator used in the data frame. The default is ",".
+    encoding : str, optional
+        The file's encoding, used to avoid errors when reading the file. The
+        default is "utf-8".
+
+    Notes
+    -----
+    Try to avoid using forbidden characters in the column names, such as
+    parenthesis, brackets, etc. These characters may cause errors when
+    accessing the attributes of the Flight object.
     """
 
-    name: str
-    time_col: str
-    df: pd.DataFrame
-    columns_map: dict = None
-    interpolation: str = "linear"
-    extrapolation: str = "zero"
+    def __init__(
+        self,
+        name,
+        filepath,
+        columns_map=None,
+        units=None,
+        interpolation="linear",
+        extrapolation="zero",
+        separator=",",
+        encoding="utf-8",
+    ):
+        self.name = name
+        self.filepath = filepath
+        self.columns_map = columns_map
+        self.units = units
+        self.interpolation = interpolation
+        self.extrapolation = extrapolation
+        self.separator = separator
+        self.encoding = encoding
+
+        self.columns = None
+        self.data = None
+        self.time_col = None
+
+        self.__handle_data()
+        self.__determine_time_column()
+        self.__create_attributes()
 
     def __repr__(self):
-        return (
-            f"'PostFlight object: name = '{self.name}', time_col = '{self.time_col})''"
+        return f"FlightDataImporter object: '{self.name}'"
+
+    def __handle_data(self):
+        raw_data = np.genfromtxt(
+            self.filepath,
+            delimiter=self.separator,
+            encoding=self.encoding,
+            dtype=np.float64,
+            names=True,
+            deletechars="",
         )
 
-    @cached_property
-    def time(self):
-        """An np.ndarray containing the time values of the flight. The time
-        values are taken from the data frame so they have the same units as the
-        time column in the data frame. It is recommended to use time in seconds.
-        """
-        return self.df[self.time_col].values
+        # Store the column names
+        self.original_columns = raw_data.dtype.names
+        self.data = raw_data.view((np.float64, len(self.original_columns)))
 
-    @df2property("x", "X position (m)", interpolation, extrapolation)
-    def x(self):
-        pass
+        # Create the columns map if necessary
+        if not self.columns_map:
+            self.columns_map = {col: col for col in self.original_columns}
 
-    @df2property("y", "Y position (m)", interpolation, extrapolation)
-    def y(self):
-        pass
+        # Map original columns to their new names (if mapped), otherwise keep as is
+        self.columns = [self.columns_map.get(col, col) for col in self.original_columns]
 
-    @df2property("z", "Z position (m)", interpolation, extrapolation)
-    def z(self):
-        pass
+    def __determine_time_column(self):
+        if self.columns_map:
+            for col, atr in self.columns_map.items():
+                if atr == "time":
+                    self.time_col = col
+                    return None
+        if "time" in self.original_columns:
+            self.time_col = "time"
+            return None
+        raise ValueError("Unable to determine the time column...")
 
-    @df2property("vx", "X velocity (m/s)", interpolation, extrapolation)
-    def vz(self):
-        pass
+    def __create_attributes(self):
+        # Extract time values
+        try:
+            times = self.data[:, self.original_columns.index(self.time_col)]
+        except ValueError as e:
+            raise ValueError(
+                f"Unable to find column '{self.time_col}' in the header of the file."
+                + "The available columns are:"
+                + str(self.original_columns)
+            ) from e
 
-    @df2property("altitude", "Altitude (m)", interpolation, extrapolation)
-    def altitude(self):
-        pass
+        created = []
+        for col, name in self.columns_map.items():
+            if name == "time":  # Handle time separately
+                setattr(self, name, times)
+                created.append(name)
+                continue
 
-    @df2property("vy", "Y velocity (m/s)", interpolation, extrapolation)
-    def vy(self):
-        pass
+            # Find the index of the current column in the filtered data
+            try:
+                col_idx = self.original_columns.index(col)
+            except ValueError:
+                warnings.warn(
+                    f"Unable to find column '{col}' in the header of the file. "
+                    + f"The attribute '{name}' won't be set. The available "
+                    + "columns are:"
+                    + str(self.original_columns),
+                    UserWarning,
+                )
+                continue
 
-    @df2property("vz", "Z velocity (m/s)", interpolation, extrapolation)
-    def vz(self):
-        pass
+            # Extract values for the current column
+            values = self.data[:, col_idx]
 
-    @df2property("ax", "X acceleration (m/s^2)", interpolation, extrapolation)
-    def ax(self):
-        pass
+            # Convert units if necessary
+            if self.units and col in self.units:
+                values /= UNITS_CONVERSION_DICT[self.units[col]]
+                print(f"Attribute '{name}' converted from {self.units[col]} to SI")
 
-    @df2property("ay", "Y acceleration (m/s^2)", interpolation, extrapolation)
-    def ay(self):
-        pass
+            # Create Function object and set as attribute
+            setattr(
+                self,
+                name,
+                Function(
+                    np.column_stack((times, values)),
+                    "Time (s)",
+                    FLIGHT_LABEL_MAP.get(name, name),
+                    self.interpolation,
+                    self.extrapolation,
+                ),
+            )
+            created.append(name)
 
-    @df2property("az", "Z acceleration (m/s^2)", interpolation, extrapolation)
-    def az(self):
-        pass
-
-    @df2property("w1", "Angular velocity ω1 (rad/s)", interpolation, extrapolation)
-    def w1(self):
-        pass
-
-    @df2property("w2", "Angular velocity ω2 (rad/s)", interpolation, extrapolation)
-    def w2(self):
-        pass
-
-    @df2property("w3", "Angular velocity ω3 (rad/s)", interpolation, extrapolation)
-    def w3(self):
-        pass
-
-    @df2property("latitude", "Latitude (deg)", interpolation, extrapolation)
-    def latitude(self):
-        pass
-
-    @df2property("longitude", "Longitude (deg)", interpolation, extrapolation)
-    def longitude(self):
-        pass
-
-    @property
-    def apogee(self):
-        """Returns the apogee of the flight. This is calculated using the
-        maximum altitude of the flight.
-
-        Returns
-        -------
-        float
-            The apogee of the flight in meters.
-        """
-        return self.z.max
+        print(
+            "The following attributes were create and are now available to be used: ",
+            created,
+        )
