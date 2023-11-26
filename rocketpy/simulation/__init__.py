6f86cb7c140433cc6866d477e33c25936b6a946e
Gui-FernandesBR <guilherme_fernandes@usp.br>
2023-11-25 22:42:32 -0300

    TST: Add tests for FlightDataImporter class

--- rocketpy/simulation/__init__.py
+++ rocketpy/simulation/__init__.py
@@ -1,1 +1,2 @@
 from .flight import Flight
+from .flight_data_importer import FlightDataImporter
---
+++ tests/test_flight_data_importer.py
@@ -0,0 +1,50 @@
+"""Tests the FlightDataImporter class from rocketpy.simulation module.
+"""
+import numpy as np
+
+from rocketpy.simulation import FlightDataImporter
+
+
+def test_flight_importer_bella_lui():
+    """Tests the class using the Bella Lui flight data."""
+    columns_map = {"time_aprox_(s)": "time", "z_(m)": "altitude", "v_(m/s)": "vz"}
+    path = "tests/fixtures/acceptance/EPFL_Bella_Lui/bella_lui_flight_data_filtered.csv"
+
+    fd = FlightDataImporter(
+        name="Bella Lui, EPFL Rocket Team, 2020",
+        filepath=path,
+        columns_map=columns_map,
+        units=None,
+        interpolation="linear",
+        extrapolation="zero",
+        separator=",",
+        encoding="utf-8",
+    )
+
+    assert fd.name == "Bella Lui, EPFL Rocket Team, 2020"
+    assert "time" in fd.columns, "Can't find 'time' column in fd.columns"
+    assert "altitude" in fd.columns, "Can't find 'altitude' column in fd.columns"
+    assert "vz" in fd.columns, "Can't find 'vz' column in fd.columns"
+    assert np.isclose(fd.altitude(0), 0.201, atol=1e-4)
+    assert np.isclose(fd.vz(0), 5.028, atol=1e-4)
+
+
+def test_flight_importer_ndrt():
+    """Tests the class using the NDRT 2020 flight data."""
+    columns_map = {
+        "Time_(s)": "time",
+        "Altitude_(Ft-AGL)": "altitude",
+    }
+    units = {"Altitude_(Ft-AGL)": "ft"}
+    path = "tests/fixtures/acceptance/NDRT_2020/ndrt_2020_flight_data.csv"
+
+    fd = FlightDataImporter(
+        name="NDRT Rocket team, 2020",
+        filepath=path,
+        columns_map=columns_map,
+        units=units,
+    )
+    assert fd.name == "name"
+    assert "time" in fd.columns, "Can't find 'time' column in fd.columns"
+    assert "altitude" in fd.columns, "Can't find 'altitude' column in fd.columns"
+    assert np.isclose(fd.altitude(0), 0)
