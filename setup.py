9ac0e4fad7ae5432f9ee90c1dbbe6c09040372e0
Gui-FernandesBR <63590233+Gui-FernandesBR@users.noreply.github.com>
2024-02-22 14:51:42 -0500

    REL: bump up rocketpy version to v1.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ env_analysis_require = [
 
 setuptools.setup(
     name="rocketpy",
-    version="1.2.0",
+    version="1.2.1",
     install_requires=necessary_require,
     extras_require={
         "env_analysis": env_analysis_require,
