9198ac704f2d5cd1c1448da05c44b9a129a3b83c
Pedro Bressan <phmbressan@usp.br>
2024-02-29 17:46:23 -0300

    DOC: phrasing improvements of Environment docstring.

--- rocketpy/environment/environment.py
+++ rocketpy/environment/environment.py
@@ -276,15 +276,21 @@ class Environment:
         timezone="UTC",
         max_expected_height=80000.0,
     ):
-        """Initialize Environment class, saving launch rail length,
-        launch date, location coordinates and elevation. Note that
-        by default the standard atmosphere is loaded until another
+        """Initialize Environment class, saving parameters of the launch location, 
+        such as launch date, coordinates and elevation. This class also computes 
+        relevant quantities for the Flight simulation, such as air pressure, density 
+        and gravitational acceleration.
+        
+        Note that the default atmospheric model is the International Standard Atmosphere 
+        as defined by ISO 2533 unless specified otherwise in 
+        :meth:`Environment.set_atmospheric_model`.
 
         Parameters
         ----------
         gravity : int, float, callable, string, array, optional
             Surface gravitational acceleration. Positive values point the
-            acceleration down. If None, the Somigliana formula is used to
+            acceleration down. If None, the Somigliana formula is used. 
+            See :meth:`Environment.set_gravity_model` for more information.
         date : array, optional
             Array of length 4, stating (year, month, day, hour (UTC))
             of rocket launch. Must be given if a Forecast, Reanalysis
@@ -468,8 +474,8 @@ class Environment:
         ----------
         gravity : None or Function source
             If None, the Somigliana formula is used to compute the gravity
-            acceleration. Otherwise, the user can provide a Function object
-            representing the gravity model.
+            acceleration. Otherwise, the user can provide a Function source 
+            object representing the gravity model.
 
         Returns
         -------
@@ -514,6 +520,10 @@ class Environment:
         -------
         Function
             Function object representing the gravity model.
+
+        References
+        ----------
+        .. [1] https://en.wikipedia.org/wiki/Theoretical_gravity#Somigliana_equation
         """
         a = 6378137.0  # semi_major_axis
         f = 1 / 298.257223563  # flattening_factor
