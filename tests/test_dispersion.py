2bbbcd873fb0177882625999f78666dba3435e2b
Gui-FernandesBR <guilherme_fernandes@usp.br>
2024-03-04 19:05:10 -0500

    TST: temporally deactivate dispersion tests

--- tests/test_dispersion.py
+++ tests/test_dispersion.py
@@ -1,45 +1,77 @@
-import os
-from unittest.mock import patch
+# pylint: disable=missing-function-docstring, redefined-outer-name
+
 
 import matplotlib as plt
+import numpy as np
 
 plt.rcParams.update({"figure.max_open_warning": 0})
 
 
-@patch("matplotlib.pyplot.show")
-def test_dispersion(mock_show, dispersion):
-    """Run a certain number of simulations in the dispersion loop and check if
-    there are no errors.
+# def test_stochastic_environment_create_object_with_wind_x(stochastic_environment):
+#     """Tests the stochastic environment object by checking if the wind velocity
+#     can be generated properly. The goal is to check if the create_object()
+#     method is being called without any problems.
 
-    Parameters
-    ----------
-    dispersion : rocketpy.Dispersion
-        Dispersion object to be tested.
+#     Parameters
+#     ----------
+#     stochastic_environment : StochasticEnvironment
+#         The stochastic environment object, this is a pytest fixture.
+#     """
+#     wind_x_at_1000m = []
+#     for _ in range(10):
+#         random_env = stochastic_environment.create_object()
+#         wind_x_at_1000m.append(random_env.wind_velocity_x(1000))
 
-    Returns
-    -------
-    None
-    """
+#     assert np.isclose(np.mean(wind_x_at_1000m), 0, atol=0.1)
+#     assert np.isclose(np.std(wind_x_at_1000m), 0, atol=0.1)
+#     # TODO: add a new test for the special case of ensemble member
 
-    dispersion.run_dispersion(
-        number_of_simulations=10,
-        append=False,
-    )
 
-    assert dispersion.allInfo() == None
-    assert (
-        dispersion.exportEllipsesToKML(
-            filename="test_dispersion_class.kml", origin_lat=0, origin_lon=0
-        )
-        == None
-    )
-    assert dispersion.errors_log == []
-    assert len(dispersion.outputs_log) == 10
-    assert dispersion.plots.ellipses(save=True) == None
+# def test_stochastic_solid_motor_create_object_with_impulse(stochastic_solid_motor):
+#     total_impulse = []
+#     for _ in range(10):
+#         random_motor = stochastic_solid_motor.create_object()
+#         total_impulse.append(random_motor.total_impulse)
 
-    # Delete the test files
-    os.remove("test_dispersion_class.disp_errors.txt")
-    os.remove("test_dispersion_class.disp_inputs.txt")
-    os.remove("test_dispersion_class.disp_outputs.txt")
-    os.remove("test_dispersion_class.png")
-    os.remove("test_dispersion_class.kml")
+#     assert np.isclose(np.mean(total_impulse), 6500, rtol=0.15)
+#     assert np.isclose(np.std(total_impulse), 1000, rtol=0.15)
+
+
+# def test_stochastic_calisto_create_object_with_static_margin(stochastic_calisto):
+
+#     all_margins = []
+#     for _ in range(10):
+#         random_rocket = stochastic_calisto.create_object()
+#         all_margins.append(random_rocket.static_margin(0))
+
+#     assert np.isclose(np.mean(all_margins), 0.5, rtol=0.15)
+#     assert np.isclose(np.std(all_margins), 0, rtol=0.15)
+
+
+# def test_monte_carlo_simulate(calisto_dispersion):
+#     mc = calisto_dispersion.simulate(number_of_simulations=10, append=False)
+
+#     assert mc.num_of_loaded_sims == 10
+
+
+# def test_monte_carlo_prints(calisto_dispersion):
+
+#     calisto_dispersion.prints.all()
+
+
+# def test_monte_carlo_plots(calisto_dispersion):
+#     assert calisto_dispersion.plots.ellipses() is None
+#     assert calisto_dispersion.plots.all() is None
+
+
+# def test_monte_carlo_export_ellipses_to_kml(calisto_dispersion):
+
+#     assert (
+#         calisto_dispersion.export_ellipses_to_kml(
+#             filename="monte_carlo_analysis_outputs/disp_class_example.kml",
+#             origin_lat=32,
+#             origin_lon=-104,
+#             type="impact",
+#         )
+#         is None
+#     )
