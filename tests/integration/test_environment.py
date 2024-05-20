87e3eae5b52eb8f9b358d886f25ef3e2963a01ef
Lucas <69172945+lucasfourier@users.noreply.github.com>
2024-05-19 23:16:18 -0300

    Unclassified tests in the test_environment.py no longer exists. Final commit  and fixes to the testing modules of environment in the unit and integration folders.

--- tests/integration/test_environment.py
+++ tests/integration/test_environment.py
@@ -11,6 +11,131 @@ import pytz
 from rocketpy import Environment
 
 
+@pytest.mark.slow
+@patch("matplotlib.pyplot.show")
+def test_gfs_atmosphere(mock_show, example_spaceport_env):
+    """Tests the Forecast model with the GFS file. It does not test the values,
+    instead the test checks if the method runs without errors.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_spaceport_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_spaceport_env.set_atmospheric_model(type="Forecast", file="GFS")
+    assert example_spaceport_env.all_info() == None
+
+
+@pytest.mark.slow
+@patch("matplotlib.pyplot.show")
+def test_nam_atmosphere(mock_show, example_spaceport_env):
+    """Tests the Forecast model with the NAM file.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_spaceport_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_spaceport_env.set_atmospheric_model(type="Forecast", file="NAM")
+    assert example_spaceport_env.all_info() == None
+
+
+# Deactivated since it is hard to figure out and appropriate date to use RAP forecast
+@pytest.mark.skip(reason="legacy tests")
+@pytest.mark.slow
+@patch("matplotlib.pyplot.show")
+def test_rap_atmosphere(mock_show, example_spaceport_env):
+    today = datetime.date.today()
+    example_spaceport_env.set_date((today.year, today.month, today.day, 8))
+    example_spaceport_env.set_atmospheric_model(type="Forecast", file="RAP")
+    assert example_spaceport_env.all_info() == None
+
+
+@patch("matplotlib.pyplot.show")
+def test_era5_atmosphere(mock_show, example_spaceport_env):
+    """Tests the Reanalysis model with the ERA5 file. It uses an example file
+    available in the data/weather folder of the RocketPy repository.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_spaceport_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_spaceport_env.set_date((2018, 10, 15, 12))
+    example_spaceport_env.set_atmospheric_model(
+        type="Reanalysis",
+        file="data/weather/SpaceportAmerica_2018_ERA-5.nc",
+        dictionary="ECMWF",
+    )
+    assert example_spaceport_env.all_info() == None
+
+
+@pytest.mark.slow
+@patch("matplotlib.pyplot.show")
+def test_gefs_atmosphere(mock_show, example_spaceport_env):
+    """Tests the Ensemble model with the GEFS file.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_spaceport_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_spaceport_env.set_atmospheric_model(type="Ensemble", file="GEFS")
+    assert example_spaceport_env.all_info() == None
+
+
+@patch("matplotlib.pyplot.show")
+def test_custom_atmosphere(mock_show, example_plain_env):
+    """Tests the custom atmosphere model in the environment object.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_plain_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_plain_env.set_atmospheric_model(
+        type="custom_atmosphere",
+        pressure=None,
+        temperature=300,
+        wind_u=[(0, 5), (1000, 10)],
+        wind_v=[(0, -2), (500, 3), (1600, 2)],
+    )
+    assert example_plain_env.all_info() == None
+    assert abs(example_plain_env.pressure(0) - 101325.0) < 1e-8
+    assert abs(example_plain_env.barometric_height(101325.0)) < 1e-2
+    assert abs(example_plain_env.wind_velocity_x(0) - 5) < 1e-8
+    assert abs(example_plain_env.temperature(100) - 300) < 1e-8
+
+
+@patch("matplotlib.pyplot.show")
+def test_standard_atmosphere(mock_show, example_plain_env):
+    """Tests the standard atmosphere model in the environment object.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_plain_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_plain_env.set_atmospheric_model(type="standard_atmosphere")
+    assert example_plain_env.info() == None
+    assert example_plain_env.all_info() == None
+    assert abs(example_plain_env.pressure(0) - 101325.0) < 1e-8
+    assert abs(example_plain_env.barometric_height(101325.0)) < 1e-2
+    assert example_plain_env.prints.print_earth_details() == None
+
+
 @patch("matplotlib.pyplot.show")
 def test_wyoming_sounding_atmosphere(mock_show, example_plain_env):
     """Asserts whether the Wyoming sounding model in the environment
@@ -181,3 +306,19 @@ def test_environment_export_environment_exports_valid_environment_json(
     )
 
     os.remove("environment.json")
+
+
+@pytest.mark.slow
+@patch("matplotlib.pyplot.show")
+def test_cmc_atmosphere(mock_show, example_spaceport_env):
+    """Tests the Ensemble model with the CMC file.
+
+    Parameters
+    ----------
+    mock_show : mock
+        Mock object to replace matplotlib.pyplot.show() method.
+    example_spaceport_env : rocketpy.Environment
+        Example environment object to be tested.
+    """
+    example_spaceport_env.set_atmospheric_model(type="Ensemble", file="CMC")
+    assert example_spaceport_env.all_info() == None
