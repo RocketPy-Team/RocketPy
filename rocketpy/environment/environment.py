e88bc91139b8c246b6f1445258a253a74ea29984
Gui-FernandesBR <guilherme_fernandes@usp.br>
2024-05-19 22:40:32 -0300

    MNT: minor fix to the Environment.set_earth_geometry() method

--- rocketpy/environment/environment.py
+++ rocketpy/environment/environment.py
@@ -2700,8 +2700,6 @@ class Environment:
             "it in the future by using the custom_atmosphere atmospheric model."
         )
 
-        return None
-
     def set_earth_geometry(self, datum):
         """Sets the Earth geometry for the ``Environment`` class based on the
         datum provided.
@@ -2725,116 +2723,14 @@ class Environment:
         }
         try:
             return ellipsoid[datum]
-        except KeyError:
+        except KeyError as e:
+            available_datums = ', '.join(ellipsoid.keys())
             raise AttributeError(
-                f"The reference system {datum} for Earth geometry " "is not recognized."
-            )
+                f"The reference system '{datum}' is not recognized. Please use one of "
+                f"the following recognized datum: {available_datums}"
+            ) from e
 
-    # Auxiliary functions - Fetching Data from 3rd party APIs
-
-    @exponential_backoff(max_attempts=3, base_delay=1, max_delay=60)
-    def __fetch_open_elevation(self):
-        print("Fetching elevation from open-elevation.com...")
-        request_url = (
-            "https://api.open-elevation.com/api/v1/lookup?locations"
-            f"={self.latitude},{self.longitude}"
-        )
-        try:
-            response = requests.get(request_url)
-        except Exception as e:
-            raise RuntimeError("Unable to reach Open-Elevation API servers.")
-        results = response.json()["results"]
-        return results[0]["elevation"]
-
-    @exponential_backoff(max_attempts=5, base_delay=2, max_delay=60)
-    def __fetch_atmospheric_data_from_windy(self, model):
-        model = model.lower()
-        if model[-1] == "u":  # case iconEu
-            model = "".join([model[:4], model[4].upper(), model[4 + 1 :]])
-        url = (
-            f"https://node.windy.com/forecast/meteogram/{model}/"
-            f"{self.latitude}/{self.longitude}/?step=undefined"
-        )
-        try:
-            response = requests.get(url).json()
-        except Exception as e:
-            if model == "iconEu":
-                raise ValueError(
-                    "Could not get a valid response for Icon-EU from Windy. "
-                    "Check if the coordinates are set inside Europe."
-                )
-        return response
-
-    @exponential_backoff(max_attempts=5, base_delay=2, max_delay=60)
-    def __fetch_wyoming_sounding(self, file):
-        response = requests.get(file)
-        if response.status_code != 200:
-            raise ImportError(f"Unable to load {file}.")
-        if len(re.findall("Can't get .+ Observations at", response.text)):
-            raise ValueError(
-                re.findall("Can't get .+ Observations at .+", response.text)[0]
-                + " Check station number and date."
-            )
-        if response.text == "Invalid OUTPUT: specified\n":
-            raise ValueError(
-                "Invalid OUTPUT: specified. Make sure the output is Text: List."
-            )
-        return response
-
-    @exponential_backoff(max_attempts=5, base_delay=2, max_delay=60)
-    def __fetch_noaaruc_sounding(self, file):
-        response = requests.get(file)
-        if response.status_code != 200 or len(response.text) < 10:
-            raise ImportError("Unable to load " + file + ".")
-
-    @exponential_backoff(max_attempts=5, base_delay=2, max_delay=60)
-    def __fetch_gefs_ensemble(self, dictionary):
-        time_attempt = datetime.now(tz=timezone.utc)
-        success = False
-        attempt_count = 0
-        while not success and attempt_count < 10:
-            time_attempt -= timedelta(hours=6 * attempt_count)
-            file = (
-                f"https://nomads.ncep.noaa.gov/dods/gens_bc/gens"
-                f"{time_attempt.year:04d}{time_attempt.month:02d}"
-                f"{time_attempt.day:02d}/"
-                f"gep_all_{6 * (time_attempt.hour // 6):02d}z"
-            )
-            try:
-                self.process_ensemble(file, dictionary)
-                success = True
-            except OSError:
-                attempt_count += 1
-        if not success:
-            raise RuntimeError(
-                "Unable to load latest weather data for GEFS through " + file
-            )
-
-    @exponential_backoff(max_attempts=5, base_delay=2, max_delay=60)
-    def __fetch_cmc_ensemble(self, dictionary):
-        # Attempt to get latest forecast
-        time_attempt = datetime.now(tz=timezone.utc)
-        success = False
-        attempt_count = 0
-        while not success and attempt_count < 10:
-            time_attempt -= timedelta(hours=12 * attempt_count)
-            file = (
-                f"https://nomads.ncep.noaa.gov/dods/cmcens/"
-                f"cmcens{time_attempt.year:04d}{time_attempt.month:02d}"
-                f"{time_attempt.day:02d}/"
-                f"cmcens_all_{12 * (time_attempt.hour // 12):02d}z"
-            )
-            try:
-                self.process_ensemble(file, dictionary)
-                success = True
-            except OSError:
-                attempt_count += 1
-        if not success:
-            raise RuntimeError(
-                "Unable to load latest weather data for CMC through " + file
-            )
-
-    # Auxiliary functions - Geodesic Coordinates
+    # Auxiliary functions - Geodesic Coordinates # TODO: move it to env.tools.py
 
     @staticmethod
     def geodesic_to_utm(
