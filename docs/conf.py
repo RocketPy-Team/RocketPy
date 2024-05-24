e859c47bd9dca659401279f5965f925a4912ef07
Pedro Bressan <phmbressan@usp.br>
2024-05-24 07:29:07 -0300

    BUG: add new PYTHONPATH to conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../"))
-
+package_path = os.path.abspath("../")
+sys.path.insert(0, package_path)
+os.environ["PYTHONPATH"] = ":".join((package_path, os.environ.get("PYTHONPATH", "")))
 
 # -- Project information -----------------------------------------------------
 
