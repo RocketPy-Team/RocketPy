a29e5f665cfc001ddbc65fa1538825fe13074853
Lucas de Oliveira Prates <lucasdelprates@gmail.com>
2024-06-10 21:26:29 -0300

    MNT: applying review suggestions to sensitivity analysis.
    Fix code style issues with Black
    
    BUG: fixing var and std swap mistake in calculations
    
    BUG: fixing incorrect print of regressions coefficients on summary table.
    
    DOC: adding sensitivity analysis data and simulation notebook.
    
    DOC: expanded sensitivity analysis usage notebook
    
    ENH: introducing ImportanceModel class for parameter importance analysis
    
    MNT: adding imports and renaming analysis folder
    
    ENH: implementing plot to ImportanceModel and fixing estimation/import errors
    
    ENH: implementing summary method to ImportanceModel
    
    MNT: adding optional requirements for ImportanceModel
    
    MNT: using optional import tools and adding sensitivity dependency install to setup.py
    
    MNT: renaming the term 'importance' to 'sensitivity' in variables, files, and folders
    
    MNT: Improving doc and input validation.
    
    ENH: implementing function in tools to extract data from MonteCarlo simulation's.
    
    MNT: adding json dependency to tools.py
    
    MNT: removing type hints for consistency with codebase (#444)
    
    MNT: applying review suggestions to sensitivity analysis.
    
    BUG: fixing var and std swap mistake in calculations

--- rocketpy/__init__.py
+++ rocketpy/__init__.py
@@ -37,6 +37,7 @@ from .rocket import (
     Tail,
     TrapezoidalFins,
 )
+from .sensitivity import SensitivityModel
 from .simulation import Flight, MonteCarlo
 from .stochastic import (
     StochasticEllipticalFins,
@@ -49,5 +50,4 @@ from .stochastic import (
     StochasticTail,
     StochasticTrapezoidalFins,
 )
-from .sensitivity import SensitivityModel
 from .tools import load_monte_carlo_data
