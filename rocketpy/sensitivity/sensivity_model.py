8bbb7c4ed8adf0b4da75f88ee4b2b2960670ddf6
Lint Action <lint-action@samuelmeuli.com>
2024-06-11 00:27:16 +0000

    Fix code style issues with Black

--- rocketpy/sensitivity/sensivity_model.py
+++ rocketpy/sensitivity/sensivity_model.py
@@ -145,9 +145,9 @@ class SensitivityModel:
                  target variables passed at initialization."
             )
         for i, target_variable in enumerate(self.target_variables_names):
-            self.target_variables_info[target_variable][
-                "nominal_value"
-            ] = target_variables_nominal_value[i]
+            self.target_variables_info[target_variable]["nominal_value"] = (
+                target_variables_nominal_value[i]
+            )
 
         self._nominal_target_passed = True
 
