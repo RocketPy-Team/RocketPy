fb2588971dc4389319520d9c0fabcf9763c5848c
Gui-FernandesBR <guilherme_fernandes@usp.br>
2024-11-30 12:39:12 -0300

    REL: update version to 1.7.0 in configuration files

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2024, RocketPy Team"
 author = "RocketPy Team"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.2"
+release = "1.7.0"
 
 
 # -- General configuration ---------------------------------------------------
