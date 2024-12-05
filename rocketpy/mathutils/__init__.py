920f15c36bd9f39b9114ed4595420a080c920e3a
Yogiraj Gutte <ygutte@nvidia.com>
2024-12-06 02:12:12 +0530

    improved import for linting

--- rocketpy/mathutils/__init__.py
+++ rocketpy/mathutils/__init__.py
@@ -3,5 +3,5 @@ from .function import (
     funcify_method,
     reset_funcified_methods,
 )
-from .vector_matrix import Matrix, Vector
 from .piecewise_function import PiecewiseFunction
+from .vector_matrix import Matrix, Vector
