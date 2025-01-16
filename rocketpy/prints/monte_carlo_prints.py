48efc871c9b7eb489055c3f6e7b76d54ead2e93e
EvanM <evan.madurai@googlemail.com>
2025-01-16 10:10:42 +0000

    ENH: Display more information in MonteCarlo prints and plots (#760)
    * init: new prints test
    
    * move new prints into mc class itself
    
    * enh: boxplots added to histograms in montecarlo plots
    
    * DOC: update changelog
    
    * DOC: fix changelog issues
    
    * ENH: cleaner calculation of result parameters
    
    * enh: cleanup monte-carlo plots
    
    * STY: linting fixes and re-format
    
    * STL: remove extra files
    
    * BUG: fix compatability with matplotlib==3.5
    
    done by removing height_ratios and replacing with gridspec
    
    ---------
    
    Co-authored-by: Evan Madurai <evanm@Senedd.local>

--- rocketpy/prints/monte_carlo_prints.py
+++ rocketpy/prints/monte_carlo_prints.py
@@ -21,10 +21,16 @@ class _MonteCarloPrints:
         print("Data Source: ", self.monte_carlo.filename)
         print("Number of simulations: ", self.monte_carlo.num_of_loaded_sims)
         print("Results: \n")
-        print(f"{'Parameter':>25} {'Mean':>15} {'Std. Dev.':>15}")
-        print("-" * 60)
+        print(
+            f"{'Parameter':>25} {'Mean':>15} {'Median':>15} {'Std. Dev.':>15} {'95% PI Lower':>15} {'95% PI Upper':>15}"
+        )
+        print("-" * 110)
         for key, value in self.monte_carlo.processed_results.items():
             try:
-                print(f"{key:>25} {value[0]:>15.3f} {value[1]:>15.3f}")
+                print(
+                    f"{key:>25} {value[0]:>15.3f} {value[1]:>15.3f} {value[2]:>15.3f} {value[3]:>15.3f} {value[4]:>15.3f}"
+                )
             except TypeError:
-                print(f"{key:>25} {str(value[0]):>15} {str(value[1]):>15}")
+                print(
+                    f"{key:>25} {'N/A':>15} {'N/A':>15} {'N/A':>15} {'N/A':>15} {'N/A':>15}"
+                )
