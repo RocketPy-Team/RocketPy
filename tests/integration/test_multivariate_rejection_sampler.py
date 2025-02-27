f565acb75ec051dac2084676ac4fd64765b80790
Lucas de Oliveira Prates <lucasdelprates@gmail.com>
2025-02-27 15:54:21 -0300

    TST: adding unit and integration tests to MRS

---
+++ tests/integration/test_multivariate_rejection_sampler.py
@@ -0,0 +1,33 @@
+# pylint: disable=unused-argument
+import os
+
+import pytest
+from scipy.stats import norm
+
+from rocketpy import MultivariateRejectionSampler
+
+
+@pytest.mark.parametrize(
+    "monte_carlo_filepath, distribution_dict",
+    [
+        (
+            "tests/fixtures/monte_carlo/example",
+            {"mass": (norm(15.5, 1).pdf, norm(15.2, 1).pdf)},
+        ),
+    ],
+)
+def test_mrs_sample_integration(monte_carlo_filepath, distribution_dict):
+    """Tests the set_inputs_log method of the MonteCarlo class.
+
+    Parameters
+    ----------
+    monte_carlo_calisto : MonteCarlo
+        The MonteCarlo object, this is a pytest fixture.
+    """
+    mrs_filepath = "mrs"
+    mrs = MultivariateRejectionSampler(monte_carlo_filepath, mrs_filepath)
+    mrs.sample(distribution_dict)
+
+    os.remove("mrs.inputs.txt")
+    os.remove("mrs.outputs.txt")
+    os.remove("mrs.errors.txt")
