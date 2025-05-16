2425f290ebbc557ac43b6296d2028d2dbc3ca0f8
ArthurJWH <arthurjwhwang@gmail.com>
2025-05-16 16:24:08 -0400

    TST: fixing the method call in the test

--- tests/integration/test_environment_analysis.py
+++ tests/integration/test_environment_analysis.py
@@ -61,4 +61,4 @@ def test_exports(mock_show, env_analysis):  # pylint: disable=unused-argument
 @pytest.mark.slow
 @patch("matplotlib.pyplot.show")
 def test_create_environment_object(mock_show, env_analysis):  # pylint: disable=unused-argument
-    assert isinstance(env_analysis.get_environment_object(), Environment)
+    assert isinstance(env_analysis.create_environment_object(), Environment)
