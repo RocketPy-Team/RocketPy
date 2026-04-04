b23ab586079d4c3a4b128eb2e6f6cb6ff063418e
zuorenchen <zuorenchen@m110.nthu.edu.tw>
2026-04-04 17:51:33 +0100

    Bug: process sensor if node._component_sensors exist instead of flight.sensor

--- rocketpy/simulation/flight.py
+++ rocketpy/simulation/flight.py
@@ -844,7 +844,7 @@ class Flight:
         phase : FlightPhase
             The current flight phase.
         """
-        if self.sensors:
+        if node._component_sensors:
             u_dot = phase.derivative(self.t, self.y_sol)
             self.__measure_sensors(node._component_sensors, u_dot)
 
